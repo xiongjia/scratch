1c580ccd828090d780dceeb51770bf76034105d6
xiongjia <lexiongjia@gmail.com>
2015-08-23 20:23:06 +0800

    add leetcode problem

--- gazlowe/README.md
+++ gazlowe/README.md
@@ -6,10 +6,13 @@ It's my algorithm tests.
 
 - Run all tests: `gazlowe --log_level=test_suite`
 - Run a specific test: `gazlowe --log_level=test_suite --run_test=<test name>`   
-  <test name> is the algorithm test (e.g. --run_test=sorting_qsort )
+  <test name> is the algorithm test (e.g. --run_test=sorting/sorting_qsort )
 - Get more options: `gazlowe --help`
 
 ## The list of the tests
 
-- sorting_qsort : The QuickSort testing
+- sorting_qsort : The QuickSort testing ( g_sorting_qsort.cxx )
+- some leetcode problems : ( g_leetcode.cxx )
+  - single_number: Single Number
+  - max_depth_btree: Maximum Depth of Binary Tree
 
--- gazlowe/src/g_leetcode.cxx
+++ gazlowe/src/g_leetcode.cxx
@@ -2,13 +2,17 @@
  * gazlowe - My algorithm tests
  */
 
+#include <algorithm>
 #include <vector>
 
 #include "boost/test/unit_test.hpp"
+#include "boost/pool/pool.hpp"
 
 namespace gazlowe
 {
-    /* Single Number: https://leetcode.com/problems/single-number/ 
+    /* Single Number:
+     * https://leetcode.com/problems/single-number/
+     *
      * Given an array of integers, every element appears twice except for one.
      * Find that single one.
      */
@@ -19,6 +23,50 @@ namespace gazlowe
             [&](int &num) { ret ^= num; });
         return ret;
     }
+
+    /* Maximum Depth of Binary Tree:
+     * https://leetcode.com/problems/maximum-depth-of-binary-tree/
+     *
+     * Given a binary tree, find its maximum depth.
+     * The maximum depth is the number of nodes along the longest path from
+     * the root node down to the farthest leaf node
+     *
+     * Definition for a binary tree node.
+     * struct TreeNode {
+     *     int val;
+     *     TreeNode *left;
+     *     TreeNode *right;
+     *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+     * };
+     */
+    class MaxDepthBTree
+    {
+    public:
+        typedef struct _TreeNode
+        {
+            int val;
+            struct _TreeNode *left;
+            struct _TreeNode *right;
+        } TreeNode;
+
+        static int maxDepth(TreeNode *root)
+        {
+            if (nullptr == root)
+            {
+                return 0;
+            }
+            return 1 + std::max(maxDepth(root->left), maxDepth(root->right));
+        }
+
+        static TreeNode* alloc_node(boost::pool<> &mpool, int val)
+        {
+            TreeNode *node = reinterpret_cast<TreeNode*>(mpool.malloc());
+            node->val = val;
+            node->left = nullptr;
+            node->right = nullptr;
+            return node;
+        }
+    };
 }
 
 BOOST_AUTO_TEST_SUITE(leetcode)
@@ -29,6 +77,39 @@ BOOST_AUTO_TEST_CASE(single_number)
     std::vector<int> testData = { 1, 1, 2, 2, 3, 3, 5 };
     int ret = gazlowe::single_number(testData);
     BOOST_CHECK(ret == 5);
+}
+
+BOOST_AUTO_TEST_CASE(max_depth_btree)
+{
+    /* Testing data:
+     *             0
+     *            / \
+     *           1   2
+     *              / \
+     *             3   4
+     *            / \
+     *           5   6
+     *          /
+     *         7
+     */
+    boost::pool<> mpool(sizeof(gazlowe::MaxDepthBTree::TreeNode));    
+    auto root = gazlowe::MaxDepthBTree::alloc_node(mpool, 0);
+    auto node = gazlowe::MaxDepthBTree::alloc_node(mpool, 1);
+    root->left = node;
+    node = gazlowe::MaxDepthBTree::alloc_node(mpool, 2);
+    root->right = node;
+    node = gazlowe::MaxDepthBTree::alloc_node(mpool, 3);
+    root->right->left = node;
+    node = gazlowe::MaxDepthBTree::alloc_node(mpool, 4);
+    root->right->right = node;
+    node = gazlowe::MaxDepthBTree::alloc_node(mpool, 5);
+    root->right->left->left = node;
+    node = gazlowe::MaxDepthBTree::alloc_node(mpool, 6);
+    root->right->left->right = node;
+    node = gazlowe::MaxDepthBTree::alloc_node(mpool, 7);
+    root->right->left->left->left = node;
+    int depth = gazlowe::MaxDepthBTree::maxDepth(root);
+    BOOST_CHECK(depth == 5);
 }
 BOOST_AUTO_TEST_SUITE_END()
 

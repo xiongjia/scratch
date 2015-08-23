428aa455a01c657316a2aea7b8190a1cd168e65f
xiongjia <lexiongjia@gmail.com>
2015-08-23 17:35:53 +0800

    update qsort

--- gazlowe/src/g_sorting_qsort.cxx
+++ gazlowe/src/g_sorting_qsort.cxx
@@ -12,8 +12,10 @@
 #include "boost/array.hpp"
 #include "boost/foreach.hpp"
 
+/* Quicksort: https://en.wikipedia.org/wiki/Quicksort */
 namespace gazlowe 
 {
+
     template<class T, std::size_t N, class C>
     void quick_sort_part(boost::array<T, N> &data, 
                          std::size_t begin, std::size_t end, C cmp)
@@ -47,22 +49,28 @@ namespace gazlowe
 
 BOOST_AUTO_TEST_CASE(sorting_qsort)
 {
+    /* Testing datum */
+    const std::size_t dataSz = 7;
+    boost::array<int, dataSz> dataNum = { 2, 5, 3, 4, 8, 10, 2 };
+    boost::array<std::string, dataSz> dataStr = {
+        "2", "1", "5", "3", "4", "5", "3"
+    };
+
     /* sorting the numbers */
-    boost::array<int, 7> dataNums1 = { 2, 5, 3, 4, 8, 10, 2 };
+    BOOST_TEST_MESSAGE("Sorting numbers array");
+    boost::array<int, dataSz> dataNums1(dataNum);
     gazlowe::quick_sort(dataNums1, std::greater<int>());
-    boost::array<int, 7> dataNums2 = { 2, 5, 3, 4, 8, 10, 2 };
+    boost::array<int, dataSz> dataNums2(dataNum);
     std::sort(dataNums2.begin(), dataNums2.end(), std::greater<int>());
     BOOST_CHECK_EQUAL_COLLECTIONS(dataNums1.begin(), dataNums1.end(),
                                   dataNums2.begin(), dataNums2.end());
 
-    boost::array<std::string, 7> dataStr1 = {
-        "2", "1", "5", "3", "4", "5", "3"
-    };
+    BOOST_TEST_MESSAGE("Sorting string array");
+    boost::array<std::string, dataSz> dataStr1(dataStr);
     gazlowe::quick_sort(dataStr1, std::less<std::string>());
-    boost::array<std::string, 7> dataStr2 = {
-        "2", "1", "5", "3", "4", "5", "3"
-    };
+    boost::array<std::string, dataSz> dataStr2(dataStr);
     std::sort(dataStr2.begin(), dataStr2.end(), std::less<std::string>());
     BOOST_CHECK_EQUAL_COLLECTIONS(dataStr1.begin(), dataStr1.end(),
                                   dataStr2.begin(), dataStr2.end());
 }
+

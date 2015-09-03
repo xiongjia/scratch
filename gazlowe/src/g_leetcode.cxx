191ac4db443f088cc5abe89e31b95fc94bc1350d
xiongjia <lexiongjia@gmail.com>
2015-09-03 20:40:15 +0800

    add leetcode problem

--- gazlowe/src/g_leetcode.cxx
+++ gazlowe/src/g_leetcode.cxx
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <vector>
+#include <set>
 
 #include "g_misc.hxx"
 #include "boost/test/unit_test.hpp"
@@ -214,6 +215,32 @@ namespace gazlowe
             }
         }
     };
+
+    /* Contains Duplicate
+     * https://leetcode.com/problems/contains-duplicate/
+     *
+     * Given an array of integers, find if the array contains any duplicates.
+     * Your function should return true if any value appears at least twice
+     * in the array, and it should return false if every element is distinct. 
+     */
+    class ContainsDuplicate
+    {
+    public:
+        static bool containsDuplicate(std::vector<int>& nums)
+        {
+            std::set<int> contains;
+            for (std::vector<int>::iterator itr = nums.begin();
+                itr != nums.end(); ++itr)
+            {
+                if (contains.find(*itr) != contains.end())
+                {
+                    return true;
+                }
+                contains.insert(*itr);
+            }
+            return false;
+        }
+    };
 }
 
 BOOST_AUTO_TEST_SUITE(leetcode)
@@ -357,5 +384,20 @@ BOOST_AUTO_TEST_CASE(linked_list_cycle)
     listNode->next = invalidList->next;
     result = gazlowe::LinkedListCycle::hasCycle(invalidList);
     BOOST_REQUIRE_EQUAL(result, true);
+}
+
+BOOST_AUTO_TEST_CASE(contains_duplicate)
+{
+    bool result = gazlowe::ContainsDuplicate::containsDuplicate(
+        std::vector<int>({ {1, 2, 3, 4, 5} }));
+    BOOST_REQUIRE_EQUAL(result, false);
+
+    result = gazlowe::ContainsDuplicate::containsDuplicate(
+        std::vector<int>({ { 1, 2, 3, 4, 1 } }));
+    BOOST_REQUIRE_EQUAL(result, true);
+
+    result = gazlowe::ContainsDuplicate::containsDuplicate(
+        std::vector<int>({}));
+    BOOST_REQUIRE_EQUAL(result, false);
 }
 BOOST_AUTO_TEST_SUITE_END()

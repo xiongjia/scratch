4f08ade4535b281dfa0f90dc49d2c0bfdf7da719
xiongjia <lexiongjia@gmail.com>
2015-09-14 00:48:50 +0800

    add leetcode problems

---
+++ gazlowe/src/g_leetcode_queue_using_stacks.cxx
@@ -0,0 +1,81 @@
+/**
+ * gazlowe - My algorithm tests
+ */
+
+#include <stack>
+
+#include "boost/shared_ptr.hpp"
+#include "boost/make_shared.hpp"
+#include "boost/test/unit_test.hpp"
+
+namespace gazlowe
+{
+    class QueueUsingStacks
+    {
+    public:
+        static boost::shared_ptr<QueueUsingStacks> Create(void)
+        {
+            return boost::make_shared<QueueUsingStacks>();
+        }
+
+    private:
+        std::stack<int> m_push;
+        std::stack<int> m_pop;
+
+        void PopStackPrepare(void)
+        {
+            if (!m_pop.empty())
+            {
+                return;
+            }
+            while (!m_push.empty())
+            {
+                m_pop.push(m_push.top());
+                m_push.pop();
+            }
+        }
+
+    public:
+        /* Push element x to the back of queue. */
+        void push(int x) 
+        {
+            m_push.push(x);
+        }
+
+        /* Removes the element from in front of queue. */
+        void pop(void)
+        {
+            PopStackPrepare();
+            m_pop.pop();
+        }
+
+        /* Get the front element. */
+        int peek(void)
+        {
+            PopStackPrepare();
+            return m_pop.top();
+        }
+
+        /* Return whether the queue is empty. */
+        bool empty(void)
+        {
+            return m_push.empty() && m_pop.empty();
+        }
+    };
+}
+
+BOOST_AUTO_TEST_SUITE(leetcode)
+BOOST_AUTO_TEST_CASE(queue_using_stacks)
+{
+    BOOST_TEST_MESSAGE("Leetcode - Implement Queue using Stacks");
+    auto queue = gazlowe::QueueUsingStacks::Create();
+    BOOST_REQUIRE_EQUAL(queue->empty(), true);
+
+    queue->push(100);
+    BOOST_REQUIRE_EQUAL(queue->peek(), 100);
+    BOOST_REQUIRE_EQUAL(queue->empty(), false);
+
+    queue->pop();
+    BOOST_REQUIRE_EQUAL(queue->empty(), true);  
+}
+BOOST_AUTO_TEST_SUITE_END()

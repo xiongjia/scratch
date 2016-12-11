dcd841346fb715ece3179f972f3076496e0b1c83
xiongjia <lexiongjia@gmail.com>
2016-12-11 13:06:36 +0800

    update jcommander tests

--- jscratch/src/main/java/scratch/JCommanderScratch.java
+++ jscratch/src/main/java/scratch/JCommanderScratch.java
@@ -1,39 +1,52 @@
 package scratch;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import lombok.Builder;
 
 /**
  * Scratch code for the JCommmander https://github.com/cbeust/jcommander
  *
+ * NOTE:
+ *   This sample is using lombok.builder.
+ *   In Java IDE (e.g. Eclipse), you need install the Lombok first.
+ *   (Run java -jar lombok.jar)
+ *
  * @author lexiongjia@gmail.com
  */
+@Builder
 public class JCommanderScratch {
-	private final JCommander jcmd = new JCommander(new Commander());
+	private final Commander cmd = new Commander();
+	private final JCommander jcmd = new JCommander(cmd);
+	private String programName = "JScratch";
 
 	public class Commander {
-		@Parameter(names = {"-verbose", "-v" }, description = "verbose mode")
+		@Parameter(names = {"--verbose", "-v" }, description = "verbose mode")
 		public boolean verbose = false;
+
+		@Parameter(names = {"--number", "-n" }, description = "Number parameter")
+		public Integer num = 0;
+
+		@Parameter(names = {"--str", "-s" }, description = "String parameter")
+		public String str = "default";
+
+		@Parameter(names = "--listStr", description = "List Strings")
+		public List<String> listStr = new ArrayList<>();
 	}
 
-	public JCommanderScratch(Builder builder) {
-		jcmd.setProgramName(builder.programName);
+	public String usage() {
+		jcmd.setProgramName(this.programName);
+		StringBuilder usage = new StringBuilder();
+		jcmd.usage(usage);
+		return usage.toString();
 	}
 
-	public void parser(String... args) {
+	public Commander parser(String... args) throws ParameterException {
 		jcmd.parse(args);
+		return this.cmd;
 	}
-
-	public static class Builder {
-		public String programName = "JScratch";
-
-		public Builder withAppName(String programName) {
-			this.programName = programName;
-			return this;
-		}
-
-		public JCommanderScratch build() {
-			return new JCommanderScratch(this);
-		}
-	 }
 }
--- jscratch/src/test/java/scratch/JCommanderScratchTest.java
+++ jscratch/src/test/java/scratch/JCommanderScratchTest.java
@@ -1,18 +1,52 @@
 package scratch;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 import com.beust.jcommander.ParameterException;
+import scratch.JCommanderScratch.JCommanderScratchBuilder;
+import scratch.JCommanderScratch.Commander;
 
 public class JCommanderScratchTest {
     @Test
-    public void cmdTest() {
-    	JCommanderScratch cmd = new JCommanderScratch.Builder()
-    			.withAppName("JScratchTest").build();
+    public void cmdTypes() {
+    	JCommanderScratch cmd = new JCommanderScratchBuilder()
+    			.programName("JScratchTest").build();
 
-    	final String[] argv = { "-verbose", "3" };
+    	final String[] argv = {
+    			"--verbose",
+    			"--number", "100",
+    			"--str", "JCommanderTest",
+    			"--listStr", "str1",
+    			"--listStr", "str2" };
+    	Commander cmdArgs = cmd.parser(argv);
+    	assertEquals(cmdArgs.num.intValue(), 100);
+    	assertEquals(cmdArgs.str, "JCommanderTest");
+    	assertTrue(cmdArgs.verbose);
+    	assertEquals(cmdArgs.listStr.size(), 2);
+    	assertTrue(cmdArgs.listStr.contains("str1"));
+    	assertTrue(cmdArgs.listStr.contains("str2"));
+    }
+
+    @Test
+    public void cmdUsage() {
+    	JCommanderScratch cmd = new JCommanderScratchBuilder()
+    			.programName("UsageTest").build();
+    	final String usage = cmd.usage();
+    	assertTrue(usage.matches("^Usage:\\sUsageTest(.|\\s)*"));
+    }
+
+    @Test
+    public void cmdInvalidParam() {
+    	JCommanderScratch cmd = new JCommanderScratchBuilder().build();
+    	final String[] argv = { "--badOption" };
     	try {
     		cmd.parser(argv);
+    		fail("Should have thrown SomeException but did not!");
     	}
-    	catch (ParameterException e) { }
+    	catch (ParameterException badOptErr) {
+    		final String errMsg = badOptErr.getMessage();
+    		final String pattern = "^Unknown option:\\s--badOption(.|\\s)*";
+        	assertTrue(errMsg.matches(pattern));
+    	}
     }
 }

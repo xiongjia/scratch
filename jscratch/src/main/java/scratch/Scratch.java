2b0e1675206994e1512f938a385271c0b94ad25d
xiongjia <lexiongjia@gmail.com>
2016-12-26 00:06:55 +0800

    add check style

--- jscratch/src/main/java/scratch/Scratch.java
+++ jscratch/src/main/java/scratch/Scratch.java
@@ -2,6 +2,7 @@ package scratch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.concurrent.*;
 
 public class Scratch {
   private static final Logger log = LoggerFactory.getLogger(Scratch.class);

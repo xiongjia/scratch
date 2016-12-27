9558ca7de28b36f2827a3c886152d4057d4e9779
xiongjia <lexiongjia@gmail.com>
2016-12-27 23:30:46 +0800

    add checkstyle gradle plugin

--- jscratch/src/main/java/scratch/Scratch.java
+++ jscratch/src/main/java/scratch/Scratch.java
@@ -2,7 +2,6 @@ package scratch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.concurrent.*;
 
 public class Scratch {
   private static final Logger log = LoggerFactory.getLogger(Scratch.class);

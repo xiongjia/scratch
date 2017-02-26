e4f532d4f5be8241ef1667899a5cb151e229f0cd
xiongjia <lexiongjia@gmail.com>
2017-02-26 12:32:47 +0800

    update feign tests

--- jscratch/src/main/java/scratch/FeignScratch.java
+++ jscratch/src/main/java/scratch/FeignScratch.java
@@ -2,12 +2,16 @@ package scratch;
 
 import feign.Client;
 import feign.Feign;
+import feign.Headers;
 import feign.Request;
 import feign.Request.Options;
 import feign.RequestLine;
 import feign.Response;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
+
+import lombok.Builder;
+import lombok.Data;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,13 +25,28 @@ class FeignScratch {
     @Override
     public Response execute(Request input, Options options) throws IOException {
       log.debug("Request: {} [{}]",  input.url(), input.method());
+      if (input.body() != null) {
+        log.debug("Sending: {}",
+            input.charset() != null ? new String(input.body(), input.charset()) : "BIN");
+      }
       return Response.create(200, "reason", input.headers(), "mock".getBytes());
     }
+  }
+
+  @Builder
+  @Data
+  public static class PostData {
+    private String data;
+    private int num;
   }
 
   public static interface TestApi {
     @RequestLine("GET /api/v1.0/ping")
     String ping();
+
+    @RequestLine("POST /api/v1.0/pong")
+    @Headers("Content-Type: application/json")
+    void pong(PostData data);
   }
 
   static void feignTest() {
@@ -40,8 +59,10 @@ class FeignScratch {
         .encoder(new GsonEncoder())
         .client(new MockClient())
         .target(TestApi.class, "http://localhost:8090");
+
     final String ping = api.ping();
     log.debug("mock api result: {}", ping);
+
+    api.pong(new PostData.PostDataBuilder().data("string").num(100).build());
   }
 }
-

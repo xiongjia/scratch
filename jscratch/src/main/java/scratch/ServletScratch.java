81785a0ade7ac59086dc53a7086c07262061c280
xiongjia <lexiongjia@gmail.com>
2017-03-08 23:56:21 +0800

    add servlet tests

---
+++ jscratch/src/main/java/scratch/ServletScratch.java
@@ -0,0 +1,23 @@
+package scratch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletScratch extends HttpServlet {
+  private static final Logger log = LoggerFactory.getLogger(ServletScratch.class);
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    log.debug("new get request");
+    response.setContentType("text/plain");
+    response.getWriter().write("hello servlet request!");
+  }
+}

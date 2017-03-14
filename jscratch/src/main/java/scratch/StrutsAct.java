5bb1c405a8c9e2eedd7f44bdc638326bbe6a275b
xiongjia <lexiongjia@gmail.com>
2017-03-14 23:09:20 +0800

    fix java style

--- jscratch/src/main/java/scratch/StrutsAct.java
+++ jscratch/src/main/java/scratch/StrutsAct.java
@@ -11,7 +11,7 @@ public class StrutsAct extends DefaultActionSupport {
 
   @Override
   public String execute() throws Exception {
-      this.data = "scratch data";
-      return "ScratchView";
+    this.data = "scratch data";
+    return "ScratchView";
   }
 }

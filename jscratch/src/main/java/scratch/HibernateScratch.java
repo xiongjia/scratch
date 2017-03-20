35f98617a4cd13ac250b7e3fddf70d4ba6e1d97e
xiongjia <lexiongjia@gmail.com>
2017-03-20 22:12:29 +0800

    add sqlite dialect

--- jscratch/src/main/java/scratch/HibernateScratch.java
+++ jscratch/src/main/java/scratch/HibernateScratch.java
@@ -2,22 +2,37 @@ package scratch;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HibernateScratch {
+  private static final Logger log = LoggerFactory.getLogger(HibernateScratch.class);
+
   /** Hibernate tests. */
   public static void testHibernate() {
+    /* default sqlite db folder is ${db_store_sqlite}
+     * Add this folder to JVM args (e.g. -Ddb_store_sqlite=f:/db
+     */
     final SessionFactory sessionFactory = new Configuration()
         .configure()
         .buildSessionFactory();
 
     final Session session = sessionFactory.openSession();
 
-    // import org.hibernate.dialect.SQLiteDialect;
-    // org.hibernate.dialect.SQLiteDialect, JdbcEnvironmentInitiator.
-    /* TODO sql tests */
+    log.debug("begin transaction");
+    final Transaction tx = session.beginTransaction();
 
+    /* insert data */
+    final UserEntity usr1 = new UserEntity(new Long(1), "user_1");
+    session.save(usr1);
+    final UserEntity usr2 = new UserEntity(new Long(2), "user_2");
+    session.save(usr2);
+
+    tx.commit();
+    log.debug("commit transaction");
     session.close();
   }
 }
---
+++ jscratch/src/main/java/scratch/UserEntity.java
@@ -0,0 +1,25 @@
+package scratch;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+  @Id
+  @Column(name = "id")
+  @Getter @Setter private Long id;
+
+  @Column(name = "name")
+  @Getter @Setter private String name;
+
+  public UserEntity(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+}

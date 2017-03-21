7d736032ba4321570347aa931cf3093c9ebe6e3e
xiong-jia.le <lexiongjia@gmail.com>
2017-03-21 15:19:39 +0800

    add jmi fs testing

--- jscratch/src/main/java/scratch/misc/FileSysScratch.java
+++ jscratch/src/main/java/scratch/misc/FileSysScratch.java
@@ -1,12 +1,35 @@
 package scratch.misc;
 
+import com.google.common.jimfs.Configuration;
+import com.google.common.jimfs.Jimfs;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class FileSysScratch {
   private static final Logger log = LoggerFactory.getLogger(FileSysScratch.class);
+
+  /** jimfs tests. */
+  public static void jimFs() throws IOException {
+    final FileSystem fs = Jimfs.newFileSystem(Configuration.unix());
+    final Path foo = fs.getPath("/foo");
+    Files.createDirectory(foo);
+    Files.list(fs.getPath("/")).forEach((path) -> {
+      log.debug("item: {}", path.toAbsolutePath().toString());
+    });
+
+    final FileSystem defaultFs = FileSystems.getDefault();
+    Files.list(defaultFs.getPath("/")).forEach((path) -> {
+      log.debug("default fs item: {}", path.toAbsolutePath().toString());
+    });
+  }
 
   /** file info. */
   public static void fileStat(String path) {

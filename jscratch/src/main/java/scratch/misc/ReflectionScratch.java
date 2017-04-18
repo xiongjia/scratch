5d713dcadc4ca292193190df65b5d9407e65e7ff
xiongjia <lexiongjia@gmail.com>
2017-04-18 21:45:49 +0800

    add java reflection tests

---
+++ jscratch/src/main/java/scratch/misc/ReflectionScratch.java
@@ -0,0 +1,33 @@
+package scratch.misc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ReflectionScratch {
+  private static final Logger log = LoggerFactory.getLogger(ReflectionScratch.class);
+
+  public static class TestObj {
+    public String testMethod(String src1, String src2) {
+      return String.format("Src = %s; %s", src1, src2);
+    }
+  }
+
+  /** reflection tests. */
+  public static void test() {
+    final Object obj = new TestObj();
+
+    try {
+      final Method method = obj.getClass().getMethod("testMethod", String.class, String.class);
+      final String ret = (String)method.invoke(obj, "test string1", "test string2");
+      log.debug("retVal = {}", ret);
+    } catch (NoSuchMethodException
+           | IllegalAccessException
+           | IllegalArgumentException
+           | InvocationTargetException  err) {
+      log.debug("Error: ", err);
+    }
+  }
+}

48a6fa1a908656be1ee59cfe5e84db41366d2789
xiong-jia.le <lexiongjia@gmail.com>
2017-04-28 16:00:21 +0800

    add test ng

---
+++ jscratch/src/main/java/scratch/unittest/TestNgScratch.java
@@ -0,0 +1,12 @@
+package scratch.unittest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestNgScratch {
+  private static final Logger log = LoggerFactory.getLogger(TestNgScratch.class);
+
+  public static void test() {
+    log.debug("TestNgScratch testing");
+  }
+}

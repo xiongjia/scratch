90773427eca625d77de7a7119cc039645d0dc1a4
xiong-jia.le <lexiongjia@gmail.com>
2017-05-03 11:06:35 +0800

    add junit ParallelRunner

--- jscratch/src/main/java/scratch/unittest/UnitTestSample.java
+++ jscratch/src/main/java/scratch/unittest/UnitTestSample.java
@@ -2,16 +2,20 @@ package scratch.unittest;
 
 import static org.junit.Assert.assertEquals;
 
+import com.googlecode.junittoolbox.ParallelRunner;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@RunWith(ParallelRunner.class)
 public class UnitTestSample {
   private static final Logger log = LoggerFactory.getLogger(UnitTestSample.class);
 

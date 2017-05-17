bbe813bb17f5ea11dde75708217eb1d6d4301d44
xiongjia <lexiongjia@gmail.com>
2017-05-17 21:32:38 +0800

    update groovy object reflection tests

---
+++ jscratch/src/main/groovy/scratch/GroovyScratch.groovy
@@ -0,0 +1,17 @@
+package scratch
+
+class GroovyScratch {
+  def get(String src) {
+    return src
+  }
+
+  def getNum() {
+    return 100
+  }
+}
+
+class GroovyExtends extends Exception {
+  def getErrorCode() {
+    return 99
+  }
+}
--- jscratch/src/main/java/scratch/misc/ReflectionScratch.java
+++ jscratch/src/main/java/scratch/misc/ReflectionScratch.java
@@ -3,6 +3,9 @@ package scratch.misc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import scratch.GroovyExtends;
+import scratch.GroovyScratch;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -12,6 +15,25 @@ public class ReflectionScratch {
   public static class TestObj {
     public String testMethod(String src1, String src2) {
       return String.format("Src = %s; %s", src1, src2);
+    }
+  }
+
+  /** reflection tests (groovy). */
+  public static void testGroovy() {
+    final GroovyScratch obj = new GroovyScratch();
+    final GroovyExtends errObj = new GroovyExtends();
+
+    try {
+      final Method method = obj.getClass().getMethod("get", String.class);
+      final String ret = (String)method.invoke(obj, "test string1");
+      log.debug("retVal = {}", ret);
+      final Method mtGetErrCode = errObj.getClass().getMethod("getErrorCode");
+      log.debug("retVal = {}", (int)mtGetErrCode.invoke(errObj));
+    } catch (NoSuchMethodException
+           | IllegalAccessException
+           | IllegalArgumentException
+           | InvocationTargetException  err) {
+      log.debug("Error: ", err);
     }
   }
 

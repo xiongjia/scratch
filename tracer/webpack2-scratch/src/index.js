a8e86bf3a0089f149b981e1724b44bb4b9983276
xiongjia <lexiongjia@gmail.com>
2017-06-09 00:05:23 +0800

    update webpack test

--- tracer/webpack2-scratch/src/index.js
+++ tracer/webpack2-scratch/src/index.js
@@ -16,6 +16,13 @@ logger.debug('app started');
 root.innerHTML = `
 <strong>Webpack2</strong> tests <br>
 Production: ${APP_PRODUCTION} <br>
+
+begin<br>
+
+<p id="t1"></p>
+
+end<br>
+
 <img src='${imgGnu}' class='test'>
 `;
 
@@ -24,3 +31,17 @@ const draggable = new Draggabilly( elem);
 draggable.on('dragEnd', (evt) => {
   logger.debug('dragend', evt);
 });
+
+
+const testElem1 = document.querySelector('#t1');
+testElem1.className = 'testClassName';
+testElem1.appendChild(document.createTextNode('abc'));
+
+import data from './assets/json/data.json';
+logger.debug('data', data);
+
+// import * as axios from 'axios';
+// axios tests
+// axios.get('http://api.fanfou.com/statuses/home_timeline.json')
+//   .then(res => logger.debug(res))
+//   .catch(err => logger.debug(err));

20fe45e95f142cc6139bf87ca2e7a47404315140
xiongjia <lexiongjia@gmail.com>
2017-06-16 00:26:54 +0800

    update server code

--- orthrus/server/src/main/java/orthrus/Orthrus.java
+++ orthrus/server/src/main/java/orthrus/Orthrus.java
@@ -1,8 +1,12 @@
 package orthrus;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Orthrus {
   public static void main(String[] args) {
-    System.out.println("gradle projects test");
+    SpringApplication.run(Orthrus.class, args);
   }
 }
 
---
+++ orthrus/server/src/main/java/orthrus/OrthrusController.java
@@ -0,0 +1,28 @@
+package orthrus;
+
+import lombok.Builder;
+import lombok.Data;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrthrusController {
+  @Builder
+  @Data
+  public static class TestResponseEnt {
+    private String data = "data";
+    private int num = 100;
+  }
+
+  /** Index request. */
+  @RequestMapping(value = "/test", method = RequestMethod.GET)
+  public ResponseEntity<TestResponseEnt> index() {
+    return new ResponseEntity<TestResponseEnt>(new TestResponseEnt.TestResponseEntBuilder()
+        .data("SprintTest").num(666).build(),
+        HttpStatus.OK);
+  }
+}

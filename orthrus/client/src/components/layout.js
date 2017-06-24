3a04210833d94b40b337a4f21c18bb4bb6ceaca1
xiongjia <lexiongjia@gmail.com>
2017-06-25 01:57:43 +0800

    add redux

--- orthrus/client/src/components/layout.js
+++ orthrus/client/src/components/layout.js
@@ -13,14 +13,25 @@ export default class Layout extends React.Component {
 
   constructor() {
     super();
+    this.state = {
+      testState: 'test state',
+    };
   }
 
   shouldComponentUpdate () {
-    return false;
+    return true;
   }
 
   render() {
     logger.debug('props: ', this.props);
-    return (<div><h1>{'state data: '}</h1>{this.props.data}</div>);
+    setTimeout(() => {
+      logger.debug('updating state');
+      this.setState({testState: '123'});
+    }, 1000 * 5);
+    return (
+      <div>
+        <h1>{'prop data: '}</h1>{this.props.data}
+        <h1>{'state data:'}</h1>{this.state.testState}
+      </div>);
   }
 }

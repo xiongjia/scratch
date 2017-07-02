4f327adcc80fec5d66bf4244ee3f99376342f7a9
xiongjia <lexiongjia@gmail.com>
2017-07-02 17:57:40 +0800

    add collection tests

--- jscratch/src/main/java/scratch/misc/CommonsScratch.java
+++ jscratch/src/main/java/scratch/misc/CommonsScratch.java
@@ -2,13 +2,23 @@ package scratch.misc;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Strings;
+import com.google.common.base.Supplier;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Table;
+import com.google.common.collect.Tables;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
+import com.google.common.primitives.UnsignedLong;
+
 
 import org.apache.commons.lang3.StringUtils;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
 
 public class CommonsScratch {
   private static final Logger log = LoggerFactory.getLogger(CommonsScratch.class);
@@ -53,5 +63,29 @@ public class CommonsScratch {
       }
     });
     bus.post("hello");
+  }
+
+  /** collections. */
+  public static void testCollections() {
+    final List<String> listStr = Lists.newArrayList("test1", "test2", "test3");
+    log.debug("strings[{}]: {}", listStr.size(), listStr);
+    log.debug("strings[{}]: {}", listStr.size(), Lists.reverse(listStr));
+
+    final Map<UnsignedLong, String> mapStr = Maps.newHashMap();
+    mapStr.put(UnsignedLong.valueOf(1), "abc");
+    log.debug("map val: {}", mapStr.get(UnsignedLong.valueOf(1)));
+
+    final Table<String, String, Integer> tab = Tables.newCustomTable(
+        Maps.<String, Map<String, Integer>>newLinkedHashMap(),
+        new Supplier<Map<String, Integer>>() {
+          @Override
+          public Map<String, Integer> get() {
+            return Maps.newLinkedHashMap();
+          }
+        });
+    tab.put("r1", "c1", 100);
+    log.debug("tab val: {}", tab.get("r1", "c1"));
+    log.debug("tab val: {}", tab.row("r1"));
+    log.debug("tab val: {}", tab.column("c1"));
   }
 }

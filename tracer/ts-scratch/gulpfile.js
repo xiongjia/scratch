5b45e1fc592b71e8c5b8de6a9145f5cd5a8144fb
xiongjia <lexiongjia@gmail.com>
2017-07-04 22:37:13 +0800

    add ts lint task

--- tracer/ts-scratch/gulpfile.js
+++ tracer/ts-scratch/gulpfile.js
@@ -2,7 +2,17 @@
 
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const tslint = require('gulp-tslint');
+
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('default', () => tsProject.src()
-    .pipe(tsProject()).js.pipe(gulp.dest('dist')));
+gulp.task('compile', ['tslint'], () => tsProject.src()
+          .pipe(tsProject()).js.pipe(gulp.dest('dist')));
+
+gulp.task('tslint', () => tsProject.src()
+          .pipe(tslint({
+            configuration: './tslint.json',
+            formatter: 'prose'
+          })).pipe(tslint.report({ emitError: true })));
+
+gulp.task('default', ['compile']);

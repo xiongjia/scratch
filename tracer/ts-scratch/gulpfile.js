a692fe1c9065c301fd5c98fdc526234f57890252
xiongjia <lexiongjia@gmail.com>
2017-07-04 23:24:37 +0800

    add eslint task

--- tracer/ts-scratch/gulpfile.js
+++ tracer/ts-scratch/gulpfile.js
@@ -3,16 +3,22 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const tslint = require('gulp-tslint');
+const eslint = require('gulp-eslint');
 
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('compile', ['tslint'], () => tsProject.src()
-          .pipe(tsProject()).js.pipe(gulp.dest('dist')));
+gulp.task('compile', ['tslint', 'eslint'], () => tsProject.src()
+  .pipe(tsProject()).js.pipe(gulp.dest('dist')));
 
 gulp.task('tslint', () => tsProject.src()
-          .pipe(tslint({
-            configuration: './tslint.json',
-            formatter: 'prose'
-          })).pipe(tslint.report({ emitError: true })));
+  .pipe(tslint({
+    configuration: './tslint.json',
+    formatter: 'prose'
+  })).pipe(tslint.report({ emitError: true })));
+
+gulp.task('eslint', () => gulp.src(['gulpfile.js'])
+  .pipe(eslint('.eslintrc.json'))
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError()));
 
 gulp.task('default', ['compile']);

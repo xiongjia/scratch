5ebf65fde0414669ac3a27ea74cc81ef8dac898b
xiongjia <lexiongjia@gmail.com>
2017-07-05 20:54:45 +0800

    uadd tsfmt config

--- tracer/ts-scratch/gulpfile.js
+++ tracer/ts-scratch/gulpfile.js
@@ -4,11 +4,12 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const tslint = require('gulp-tslint');
 const eslint = require('gulp-eslint');
+const jsonlint = require('gulp-jsonlint');
 
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('compile', ['tslint', 'eslint'], () => tsProject.src()
-  .pipe(tsProject()).js.pipe(gulp.dest('dist')));
+gulp.task('compile', ['jsonlint', 'tslint', 'eslint'],
+  () => tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist')));
 
 gulp.task('tslint', () => tsProject.src()
   .pipe(tslint({
@@ -20,5 +21,10 @@ gulp.task('eslint', () => gulp.src(['gulpfile.js'])
   .pipe(eslint('.eslintrc.json'))
   .pipe(eslint.format())
   .pipe(eslint.failAfterError()));
+
+gulp.task('jsonlint', () => gulp.src(['./*.json'])
+  .pipe(jsonlint())
+  .pipe(jsonlint.reporter())
+  .pipe(jsonlint.failOnError()));
 
 gulp.task('default', ['compile']);

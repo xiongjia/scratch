08c96e64169d5816a9ad8b4d38748a21e22fc628
xiong-jia.le <lexiongjia@gmail.com>
2017-07-24 10:59:34 +0800

    add elisp tests

--- elisp-exec/scripts/test.el
+++ elisp-exec/scripts/test.el
@@ -1,3 +1,21 @@
 ; elips tests
-(princ "hello elisp\n")
+(message "elisp tests: ")
+
+; buffer filename
+(message "buffFilename: %s "
+  load-file-name (buffer-file-name))
+
+(message "folder: %s" (file-name-directory "/abc/1.txt"))
+(message "folder: %s" (file-name-directory "/abc/test/"))
+
+; strings
+(message "substr: %s" (substring "0123456789" 0 -1))
+(message "and: %s" (and nil "abc"))
+
+; project root
+(defvar app-root-path
+  (file-name-directory
+    (substring (file-name-directory (or load-file-name (buffer-file-name))) 0 -1))
+  "The root dir of this project")
+(message "app root: %s " app-root-path)
 

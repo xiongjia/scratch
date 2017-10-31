7d23bdcae43fe0c75e1fc5c2ce435714ca35ed63
xiongjia <lexiongjia@gmail.com>
2017-11-01 00:10:39 +0800

    hello tychus

---
+++ tychus/src/main/java/tychus/Tychus.java
@@ -0,0 +1,12 @@
+package tychus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tychus {
+  private static final Logger log = LoggerFactory.getLogger(Tychus.class);
+
+  public static void main(String[] args) {
+    log.info("Hello Tychus!");
+  }
+}

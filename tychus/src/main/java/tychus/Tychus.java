bb7086e1d88bd8216a65821b0cd00e053ad581c6
xiong-jia.le <lexiongjia@gmail.com>
2017-11-21 13:10:50 +0800

    update netty web codec

--- tychus/src/main/java/tychus/Tychus.java
+++ tychus/src/main/java/tychus/Tychus.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 
 import tychus.discard.DiscardServer;
 import tychus.echo.EchoServer;
+import tychus.web.HttpServer;
 
 public class Tychus {
   private static final Logger log = LoggerFactory.getLogger(Tychus.class);
 
+  /** testing servers. */
   public static void main(String[] args) throws Exception {
     final String servType = (args.length == 0) ? "discard" : args[0];
     final int servPort = 8887;
@@ -19,6 +21,9 @@ public class Tychus {
     } else if (servType.equals("echo")) {
       log.debug("testing echo server");
       new EchoServer(servPort).run();
+    } else if (servType.equals("http")) {
+      log.debug("testing http server");
+      new HttpServer(servPort).run();
     } else {
       log.warn("bad server type: {}", servType);
     }

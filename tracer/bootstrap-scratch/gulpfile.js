fc015a347b0d56b52f9ae90a59ce35bc742b4e75
xiongjia <lexiongjia@gmail.com>
2018-01-10 22:57:03 +0800

    update bootstrap tests

--- tracer/bootstrap-scratch/gulpfile.js
+++ tracer/bootstrap-scratch/gulpfile.js
@@ -1,37 +1,27 @@
 'use strict';
 
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const inject = require('gulp-inject');
-const wiredep = require('wiredep').stream;
 
-gulp.task('clean', (cb) => require('del')(['dist'], cb));
+const config = {
+  debug: true
+};
 
-gulp.task('styles', () => {
-  const injFiles = gulp.src('src/styles/*.scss', { read: false });
-  const injOpts = {
-    transform: (filepath) => ('@import "' + filepath + '";'),
-    addRootSlash: false,
-    starttag: '// inject:app',
-    endtag: '// endinject'
+
+gulp.task('sass', () => {
+  const sass = require('gulp-sass');
+  const sassOpt = {
+    outputStyle: 'nested',
+    precison: 3,
+    errLogToConsole: true,
+    includePaths: ['node_modules/bootstrap-sass/assets/stylesheets']
   };
-
-  return gulp.src('src/main.scss')
-    .pipe(wiredep())
-    .pipe(inject(injFiles, injOpts))
-    .pipe(sass())
+  return gulp.src(['src/main.scss'])
+    .pipe(sass(sassOpt))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('html', ['styles'], () => {
-  const injectFiles = gulp.src(['dist/main.css']);
-  const injectOptions = {
-    addRootSlash: false,
-    ignorePath: ['src', 'dist']
-  };
-
-  return gulp.src('src/index.html')
-    .pipe(inject(injectFiles, injectOptions))
+gulp.task('fonts', () => {
+  const src = ['node_modules/bootstrap-sass/assets/fonts/**/*'];
+  return gulp.src(src)
     .pipe(gulp.dest('dist'));
 });
-

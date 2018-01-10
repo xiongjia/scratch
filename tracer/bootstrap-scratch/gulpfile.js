77cc32a638e11523c86a3bcce3d47478040ad476
xiongjia <lexiongjia@gmail.com>
2018-01-11 00:30:54 +0800

    update bootstrap tests

--- tracer/bootstrap-scratch/gulpfile.js
+++ tracer/bootstrap-scratch/gulpfile.js
@@ -2,22 +2,34 @@
 
 const gulp = require('gulp');
 
-const config = {
-  debug: true
+const conf = {
+  DEBUG: true
 };
 
+const dirs = {
+  DEST: 'dist',
+  DEST_CSS_MAP: '.'
+};
 
 gulp.task('sass', () => {
+  const sourcemaps = require('gulp-sourcemaps');
   const sass = require('gulp-sass');
+  const gulpif = require('gulp-if');
+  const minifyCss = require('gulp-minify-css');
+
   const sassOpt = {
     outputStyle: 'nested',
     precison: 3,
     errLogToConsole: true,
     includePaths: ['node_modules/bootstrap-sass/assets/stylesheets']
   };
+
   return gulp.src(['src/main.scss'])
+    .pipe(gulpif(conf.DEBUG, sourcemaps.init()))
     .pipe(sass(sassOpt))
-    .pipe(gulp.dest('dist'));
+    .pipe(minifyCss())
+    .pipe(gulpif(conf.DEBUG, sourcemaps.write(dirs.DEST_CSS_MAP)))
+    .pipe(gulp.dest(dirs.DEST));
 });
 
 gulp.task('fonts', () => {

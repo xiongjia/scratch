4f3f192f16bc3ed4eb037dc2964d157157093ee2
xiongjia <lexiongjia@gmail.com>
2018-10-07 17:18:27 +0800

    testing service test

---
+++ portal/src/test/java/portal/DataTest.java
@@ -0,0 +1,27 @@
+package portal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Portal.class)
+public class DataTest {
+  private static final Logger logger = LoggerFactory.getLogger(DataTest.class);
+
+  @Resource
+  private DataService dataService;
+
+
+  @Test
+  public void simpleTest() {
+    final List<String> data = dataService.getAllData();
+    logger.debug("data: {}", data);
+  }
+}

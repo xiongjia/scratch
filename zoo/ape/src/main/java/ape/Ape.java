0876a3c9486701838cacd3159c3b689b2820a7fc
xiongjia <lexiongjia@gmail.com>
2018-12-05 17:46:46 +0800

    hello zoo

---
+++ zoo/ape/src/main/java/ape/Ape.java
@@ -0,0 +1,14 @@
+package ape;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Ape {
+  private static final Logger log = LoggerFactory.getLogger(Ape.class);
+
+
+  /** Ape tests. */
+  public static void main(String[] args) {
+    log.debug("Ape tests");
+  }
+}

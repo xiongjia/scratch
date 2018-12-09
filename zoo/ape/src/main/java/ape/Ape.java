f20374ffb90786164d99f9ec58bb202f7eb37317
xiongjia <lexiongjia@gmail.com>
2018-12-09 20:54:54 +0800

    adding tests

--- zoo/ape/src/main/java/ape/Ape.java
+++ zoo/ape/src/main/java/ape/Ape.java
@@ -2,13 +2,16 @@ package ape;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Ape {
   private static final Logger log = LoggerFactory.getLogger(Ape.class);
-
 
   /** Ape tests. */
   public static void main(String[] args) {
     log.debug("Ape tests");
+    SpringApplication.run(Ape.class, args);
   }
 }

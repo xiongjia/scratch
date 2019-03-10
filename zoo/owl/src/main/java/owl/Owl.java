57c6e91a36a2e41b1d60090aebc20ec66c57e1bc
xiongjia <lexiongjia@gmail.com>
2019-03-10 23:54:51 +0800

    adding swagger and api tests

--- zoo/owl/src/main/java/owl/Owl.java
+++ zoo/owl/src/main/java/owl/Owl.java
@@ -4,10 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+// @EnableFeignClients
 public class Owl {
   private static final Logger log = LoggerFactory.getLogger(Owl.class);
 
--- zoo/owl/src/main/java/owl/OwlController.java
+++ zoo/owl/src/main/java/owl/OwlController.java
@@ -1,23 +1,35 @@
 package owl;
 
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Size;
 
 @RestController
+@Validated
 public class OwlController {
   private static final Logger log = LoggerFactory.getLogger(OwlController.class);
 
-  @Autowired
-  Elephant elephant;
+  // @Autowired
+  // Elephant elephant;
 
-  @RequestMapping("/")
-  public String index() {
-    log.debug("a testing api");
-    final String result = elephant.getData();
-    log.debug("result {}", result);
-    return result;
+  @ApiOperation(value = "scratch", notes = "notes")
+  @RequestMapping(value = "/test", method = RequestMethod.GET)
+  @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+  public String index(@RequestParam(required = false) @Size(min = 1, max = 5) String name) {
+    if (name == null) {
+      throw new IllegalArgumentException("The 'name' parameter must not be null or empty");
+    }
+    return name;
+    // log.debug("a testing api");
+    // final String result = elephant.getData();
+    // log.debug("result {}", result);
+    // return result;
+
+    // return "test";
   }
 }

a203efed8af25b9caa55f3807dc519b399abf328
xiongjia <lexiongjia@gmail.com>
2019-03-12 23:35:40 +0800

    add web security tests

--- zoo/owl/src/main/java/owl/OwlController.java
+++ zoo/owl/src/main/java/owl/OwlController.java
@@ -4,13 +4,12 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.constraints.Size;
 
-@RestController
-@Validated
+
+@Controller
 public class OwlController {
   private static final Logger log = LoggerFactory.getLogger(OwlController.class);
 
@@ -21,15 +20,20 @@ public class OwlController {
   @RequestMapping(value = "/test", method = RequestMethod.GET)
   @ResponseStatus(code = HttpStatus.BAD_REQUEST)
   public String index(@RequestParam(required = false) @Size(min = 1, max = 5) String name) {
-    if (name == null) {
-      throw new IllegalArgumentException("The 'name' parameter must not be null or empty");
-    }
-    return name;
+//    if (name == null) {
+//      throw new IllegalArgumentException("The 'name' parameter must not be null or empty");
+//    }
+    return "home";
     // log.debug("a testing api");
     // final String result = elephant.getData();
     // log.debug("result {}", result);
     // return result;
 
     // return "test";
+  }
+
+  @RequestMapping(value = "/login", method = RequestMethod.GET)
+  public String login() {
+    return "login";
   }
 }

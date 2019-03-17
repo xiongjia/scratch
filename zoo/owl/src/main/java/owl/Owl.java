1d4c5c3ff5fd0db6b10662ba4a8eb1da5b4d8dbd
xiongjia <lexiongjia@gmail.com>
2019-03-17 19:19:51 +0800

    adding command exec test

--- zoo/owl/src/main/java/owl/Owl.java
+++ zoo/owl/src/main/java/owl/Owl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import slug.CommandExecution;
 import slug.Slug;
 
 @SpringBootApplication
@@ -16,6 +17,10 @@ public class Owl {
     final Slug slug = new Slug();
     final String version = slug.getVersion();
     slug.listTag();
+
+    final CommandExecution cmdExec = new CommandExecution();
+    final String execResult = cmdExec.runTest();
+
 
     log.debug("Owl tests");
     SpringApplication.run(Owl.class, args);

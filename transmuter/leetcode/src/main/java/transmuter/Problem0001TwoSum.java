33a6c426ffd8d6127b5f41c48ebbdcef2d3ab5f2
xiongjia <lexiongjia@gmail.com>
2019-03-20 23:41:17 +0800

    adding leetcode test

--- transmuter/leetcode/src/main/java/transmuter/Problem0001TwoSum.java
+++ transmuter/leetcode/src/main/java/transmuter/Problem0001TwoSum.java
@@ -3,31 +3,35 @@ package transmuter;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Problem 1: Two Sum
+/* Problem 1: Two Sum (easy） https://leetcode.com/problems/two-sum/
+ *
  * Given an array of integers, return indices of the two numbers
  * such that they add up to a specific target.
  * You may assume that each input would have exactly one solution,
  * and you may not use the same element twice.
+ *
  * Example:
  *  Given nums = [2, 7, 11, 15], target = 9,
  *  Because nums[0] + nums[1] = 2 + 7 = 9,
  *  return [0, 1].
  */
+@SuppressWarnings("WeakerAccess")
 public class Problem0001TwoSum {
-  /** Problem 1: Two Sum. */
+  /** Solution for problem "two sum". */
   public int[] twoSum(int[] nums, int target) {
-    final Map<Integer, Integer> map = new HashMap();
-    int[] result = new int[2];
+    final Map<Integer, Integer> map = new HashMap<>();
+
     for (int i = 0; i < nums.length; i++) {
-      if (map.containsKey(target - nums[i])) {
-        result[0] = map.get(target - nums[i]);
-        result[1] = i;
-        break;
-      } else {
+      if (!map.containsKey(target - nums[i])) {
         map.put(nums[i], i);
+        continue;
       }
+
+      final int[] result = new int[2];
+      result[0] = map.get(target - nums[i]);
+      result[1] = i;
+      return result;
     }
-    return result;
+    return null;
   }
 }
---
+++ transmuter/leetcode/src/main/java/transmuter/Problem0002AddTwoNumbers.java
@@ -0,0 +1,18 @@
+package transmuter;
+
+/*
+ * Problem 2: Add Two Numbers (medium) https://leetcode.com/problems/add-two-numbers/
+ *
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+ *
+ * Example:
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
+public class Problem0002AddTwoNumbers {
+
+
+}

a34d98582196bcaf34a3f0c078cbeaae9e252f69
xiongjia <lexiongjia@gmail.com>
2019-04-02 00:54:09 +0800

    add scratch

--- jblank/src/main/java/jblank/Scratch.java
+++ jblank/src/main/java/jblank/Scratch.java
@@ -1,4 +1,4 @@
-package scratch;
+package jblank;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- jblank/src/test/java/jblank/ScratchTest.java
+++ jblank/src/test/java/jblank/ScratchTest.java
@@ -1,4 +1,4 @@
-package scratch;
+package jblank;
 
 import static org.junit.Assert.assertEquals;
 

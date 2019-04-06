5b7029c1785ecb71091032b50b5cd8424795de7e
xiongjia <lexiongjia@gmail.com>
2019-04-06 22:11:50 +0800

    adding more tests

--- zoo/owl/src/main/java/owl/Owl.java
+++ zoo/owl/src/main/java/owl/Owl.java
@@ -4,23 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import slug.CommandExecution;
 import slug.Slug;
 
 @SpringBootApplication
-// @EnableFeignClients
 public class Owl {
   private static final Logger log = LoggerFactory.getLogger(Owl.class);
 
   /** Elephant tests. */
   public static void main(String[] args) {
-    final Slug slug = new Slug();
-    final String version = slug.getVersion();
-    slug.listTag();
-
-    final CommandExecution cmdExec = new CommandExecution();
-    final String execResult = cmdExec.runTest();
-
+    log.debug("version: {}", Slug.getVersion().toString());
 
     log.debug("Owl tests");
     SpringApplication.run(Owl.class, args);
--- zoo/slug/src/main/java/slug/Slug.java
+++ zoo/slug/src/main/java/slug/Slug.java
@@ -1,35 +1,19 @@
 package slug;
 
-import java.lang.annotation.Annotation;
+import slug.general.SlugConfig;
+import slug.general.SlugVersion;
 
 public class Slug {
+  private static class Instance {
+    public static final SlugConfig CONFIG;
+
+    static {
+      CONFIG = SlugConfig.load();
+    }
+  }
 
   /** Slug tests. */
-  public String getVersion() {
-    return "version 1.1";
-  }
-
-
-  @TestTag(name = "name1", age = 11)
-  public void test() {
-
-  }
-
-  public void listTag() {
-    try {
-      final Class aClass = Class.forName("slug.Slug");
-      final Annotation[] annotations = aClass.getMethod("test").getAnnotations();
-      System.out.println(annotations);
-      for (final Annotation annotation : annotations) {
-        System.out.println(annotation);
-        if (annotation instanceof TestTag) {
-          final TestTag tag = (TestTag)annotation;
-          System.out.println("tag.name() " + tag.name());
-          System.out.println("tag.age() " + tag.age());
-        }
-      }
-    } catch (NoSuchMethodException | ClassNotFoundException e) {
-      e.printStackTrace();
-    }
+  public static SlugVersion getVersion() {
+    return Instance.CONFIG.getVersion();
   }
 }

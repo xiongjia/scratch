064869447f1cc3da6b8b712d3b8c8b7948b7b3f0
xiongjia <lexiongjia@gmail.com>
2019-04-07 18:19:26 +0800

    add build script

--- mess-tin/src/mess-tin.sh
+++ mess-tin/src/mess-tin.sh
@@ -6,7 +6,9 @@ TMP_DIR="/tmp/mess-tin"
 rm -rf ${TMP_DIR}
 mkdir -p ${TMP_DIR}
 
-sed -n -e '1,/^exit 0$/!p' $0  | tar zxv ${TMP_DIR} 2>/dev/null
+sed -n -e '1,/^exit 0$/!p' $0  | tar zxv -C ${TMP_DIR} 2>/dev/null
 cd ${TMP_DIR} && python mess-tin.py $@
-rm -rf ${TMP_DIR}
+
+# remove folders (keep it for debug)
+# rm -rf ${TMP_DIR}
 exit 0

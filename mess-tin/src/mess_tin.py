949e283d53ec67d06fd8a74cddb4d2e2d034653c
xiongjia <lexiongjia@gmail.com>
2019-04-23 22:34:34 +0800

    adding pylintrc

---
+++ mess-tin/src/mess_tin.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+    mess_tin.py
+    ~~~~~~~~~~~~~~~~
+    installer shell script
+"""
+
+def main():
+    """Main function"""
+    print "hello: mess tin"
+
+if __name__ == "__main__":
+    main()
--- mess-tin/src/mess_tin.sh
+++ mess-tin/src/mess_tin.sh
@@ -7,7 +7,7 @@ rm -rf ${TMP_DIR}
 mkdir -p ${TMP_DIR}
 
 sed -n -e '1,/^exit 0$/!p' $0  | tar zxv -C ${TMP_DIR} 2>/dev/null
-cd ${TMP_DIR} && python mess-tin.py $@
+cd ${TMP_DIR} && python mess_tin.py $@
 
 # remove folders (keep it for debug)
 # rm -rf ${TMP_DIR}

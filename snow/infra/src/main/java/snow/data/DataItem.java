4c166237b9003946222dcd37c3434b3ba74a3f03
xiongjia <lexiongjia@gmail.com>
2019-06-30 17:06:07 +0800

    update redis

--- snow/infra/src/main/java/snow/data/DataItem.java
+++ snow/infra/src/main/java/snow/data/DataItem.java
@@ -5,7 +5,7 @@ import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 
-@RedisHash("data-item")
+@RedisHash(value = "dataitem", timeToLive = 180)
 public class DataItem implements Serializable {
   private static final long serialVersionUID = 946670577457860370L;
 
--- snow/infra/src/main/java/snow/data/DataItemRepository.java
+++ snow/infra/src/main/java/snow/data/DataItemRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.List;
 import java.util.Optional;
 
 @CrossOrigin

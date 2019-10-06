b4356b1fe3a35ecb874759ffe06793c72c5b86f2
xiongjia <lexiongjia@gmail.com>
2019-10-06 13:26:54 +0800

    adding csv export

--- snow/portal/src/main/java/snow/controller/Account.java
+++ snow/portal/src/main/java/snow/controller/Account.java
@@ -1,6 +1,13 @@
 package snow.controller;
 
+import com.google.common.net.HttpHeaders;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvException;
+import java.io.IOException;
 import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +24,17 @@ public class Account {
   @GetMapping("users")
   public List<User> getAllUsers() {
     return accountService.getAllUsers();
+  }
+
+  @GetMapping("export")
+  public void export(HttpServletResponse response) throws IOException, CsvException {
+    response.setContentType("text/csv");
+    response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=users.csv");
+    final StatefulBeanToCsv<User> csvWriter = new StatefulBeanToCsvBuilder<User>(response.getWriter())
+        .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+        .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
+        .withOrderedResults(false)
+        .build();
+    csvWriter.write(accountService.getAllUsers());
   }
 }

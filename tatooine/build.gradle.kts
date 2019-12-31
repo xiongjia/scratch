8b9b39b2031db0336d1bb612b6254a457016de90
xiongjia <lexiongjia@gmail.com>
2020-01-01 01:04:58 +0800

    updating build script

--- tatooine/build.gradle.kts
+++ tatooine/build.gradle.kts
@@ -1,10 +1,18 @@
+plugins {
+  java
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 allprojects {
   repositories {
     mavenLocal()
     mavenCentral()
   }
 }
-
 
 subprojects {
   buildDir = file("${rootProject.projectDir}/build/${project.name}")
--- tatooine/infra/build.gradle.kts
+++ tatooine/infra/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
   application
 }
 
+project.version = "10.0.0"
+
 application {
   mainClassName = "tatooine.misc.Test"
 }

790e9da217d44c308e54ebaed2464f0ec11aa96b
xiongjia <lexiongjia@gmail.com>
2020-03-15 23:13:56 +0800

    updating tests

--- hunrizi/hun-utilities/src/main/java/hun/Process.java
+++ hunrizi/hun-utilities/src/main/java/hun/Process.java
@@ -1,15 +1,18 @@
 package hun;
 
+import io.reactivex.Observable;
 import java.io.IOException;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+
+import io.reactivex.disposables.Disposable;
 import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.ProcessResult;
 import org.zeroturnaround.exec.stream.LogOutputStream;
 
 public class Process {
-  public static void main(String[] args)
-      throws InterruptedException, TimeoutException, IOException {
-    int exit = new ProcessExecutor()
+  public static void main(String[] args) throws IOException {
+    final Future<ProcessResult> future =  new ProcessExecutor()
         .command("java", "-version")
         .timeout(300, TimeUnit.SECONDS)
         .readOutput(true).redirectOutput(new LogOutputStream() {
@@ -18,7 +21,11 @@ public class Process {
             System.out.println("line: " + line);
           }
         })
-        .execute().getExitValue();
-    System.out.println(String.format("Exit %d", exit));
+        .start().getFuture();
+
+    final Disposable disposable = Observable.fromFuture(future).subscribe(
+        result -> System.out.println(String.format("Exit %d", result.getExitValue())),
+        Throwable::printStackTrace, () -> System.out.println("Done"));
+    System.out.println("exit");
   }
 }

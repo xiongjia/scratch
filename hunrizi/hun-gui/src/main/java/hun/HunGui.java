265d04009c8ef12fc9b4556af7c602949aa4c773
xiongjia <lexiongjia@gmail.com>
2020-03-15 18:38:28 +0800

    javafx tests

--- hunrizi/hun-gui/src/main/java/hun/HunGui.java
+++ hunrizi/hun-gui/src/main/java/hun/HunGui.java
@@ -1,6 +1,10 @@
 package hun;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class HunGui extends Application {
@@ -11,6 +15,20 @@ public class HunGui extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     primaryStage.setTitle("Hello");
+
+    final Button button = new Button();
+    button.setText("Test");
+    button.setOnAction(event -> {
+      final Alert alert = new Alert(Alert.AlertType.INFORMATION);
+      alert.setTitle("Info");
+      alert.setHeaderText("Header");
+      alert.setContentText("Content");
+      alert.showAndWait();
+    });
+
+    final StackPane rootPane = new StackPane();
+    rootPane.getChildren().addAll(button);
+    primaryStage.setScene(new Scene(rootPane, 300, 300));
     primaryStage.show();
   }
 }

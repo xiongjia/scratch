66f5fb642212380a4e482e753283d865304967ff
xiongjia <lexiongjia@gmail.com>
2020-03-15 16:12:26 +0800

    adding jfx tests

--- hunrizi/hun-gui/src/main/java/hun/HunGui.java
+++ hunrizi/hun-gui/src/main/java/hun/HunGui.java
@@ -1,7 +1,16 @@
 package hun;
 
-public class HunGui {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HunGui extends Application {
   public static void main(String[] args) {
-    System.out.println("test");
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    primaryStage.setTitle("Hello");
+    primaryStage.show();
   }
 }

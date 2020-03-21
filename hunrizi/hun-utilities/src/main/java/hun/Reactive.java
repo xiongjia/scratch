098fcaa9f0c85833cac525946d2186aca88479c3
xiongjia <lexiongjia@gmail.com>
2020-03-21 22:56:22 +0800

    updating jx tests

--- hunrizi/hun-utilities/src/main/java/hun/Reactive.java
+++ hunrizi/hun-utilities/src/main/java/hun/Reactive.java
@@ -8,18 +8,21 @@ public class Reactive {
   public static void main(String[] args) throws InterruptedException {
     final Observable<String> observable = Observable.just("data1", "data10");
     System.out.println(String.format("Begin: [%s]", Thread.currentThread().getName()));
-    Disposable disposable = observable.map(i -> "hdr" + i).subscribe(item -> {
+    final Disposable disposable = observable.map(i -> "hdr" + i).subscribe(item -> {
       System.out.println(String.format("Data[%s]: %s", Thread.currentThread().getName(), item));
     });
     System.out.println(String.format("End: [%s]", Thread.currentThread().getName()));
 
     System.out.println(String.format("Begin Interval: [%s]", Thread.currentThread().getName()));
     final Observable<Long> observableInterval = Observable.interval(2, TimeUnit.SECONDS);
-    disposable = observableInterval.subscribe(iterval -> {
+    System.out.println(String.format("End Interval: [%s]", Thread.currentThread().getName()));
+    final Disposable disposableInterval = observableInterval.subscribe(iterval -> {
       System.out.println(String.format("Interval [%s]: %s",
           Thread.currentThread().getName(), iterval));
     });
-    System.out.println(String.format("End Interval: [%s]", Thread.currentThread().getName()));
-    Thread.sleep(1000 * 10);
+    Thread.sleep(1000 * 3);
+    System.out.println("Interval dispose");
+    disposableInterval.dispose();
+    Thread.sleep(1000 * 3);
   }
 }

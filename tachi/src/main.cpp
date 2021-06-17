c9a51723f29d5245d5bd49ac860cec55889a7796
xiongjia <lexiongjia@gmail.com>
2021-06-17 21:28:24 +0800

    updating local test

--- tachi/src/main.cpp
+++ tachi/src/main.cpp
@@ -4,22 +4,24 @@
 
 
 #include <stdio.h>
+#include <sys/socket.h>
 
 #include "tachi-log.hxx"
 
-static void Test(void)
-{
-  TACHI_LOG_FUNCTION();
-  TACHI_LOG(info) << "Info Log in Test()";
-}
 
 int main(const int argc, const char **argv) {
   TachiLog::getInstance().init();
+  TACHI_LOG(info) << "Starting ...";
 
-  TACHI_LOG_NAMED_SCOPE("main");
-  TACHI_LOG(info) << "Info Log";
-  TACHI_LOG(error) << "Info Log";
 
-  Test();
+  // local tests 
+  
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    TACHI_LOG(error) << "  create socket error ";
+    return 1;
+  }
+
+
   return 0;
 }

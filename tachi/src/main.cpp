cf5fe8f710190796008808bf48796067cdd9ab89
xiongjia <lexiongjia@gmail.com>
2021-06-20 18:48:13 +0800

    local test

--- tachi/src/main.cpp
+++ tachi/src/main.cpp
@@ -4,7 +4,9 @@
 
 
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
 
 #include "tachi-log.hxx"
 
@@ -21,6 +23,37 @@ int main(const int argc, const char **argv) {
     TACHI_LOG(error) << "  create socket error ";
     return 1;
   }
+
+
+	struct sockaddr_in serv_addr; 
+  memset(&serv_addr, 0, sizeof(serv_addr));
+ 	serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(2333); 
+
+  int binded = bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+ 	if (binded < 0) {
+ 		TACHI_LOG(error) << "Error on binding";
+    return 1;
+ 	}
+ 
+  listen(sockfd, 100);
+  for (;;) {
+	  struct sockaddr client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    // TODO Select or other API
+    int new_sockfd = accept(sockfd, &client_addr, &client_addr_len); 
+    if (new_sockfd < 0) {
+      TACHI_LOG(error) << " invalid socket";
+      continue;
+    }
+   
+    TACHI_LOG(debug) << " new connection"; 
+    close(new_sockfd);
+  }
+
+
 
 
   return 0;

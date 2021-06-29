686587ee4c5c999e47fa31595a6b9b0c600a1d0c
xiongjia <lexiongjia@gmail.com>
2021-06-29 23:27:40 +0800

    updating autoconfig tests

--- tautotools/src/main.cpp
+++ tautotools/src/main.cpp
@@ -8,6 +8,11 @@
 
 int main(const int argc, const char **argv) {
   printf("Package =  %s (%s)\n", PACKAGE_NAME, PACKAGE_STRING);
+#ifdef HAVE_EXAMPLES 
+  printf("Examples are enabled.\n");
+#else
+  printf("Examples are disabled.\n");
+#endif
   return 0;
 }
 

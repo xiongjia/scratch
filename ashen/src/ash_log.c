ea864735dac9b36c29a4f454212aef3d3cb4831d
xiongjia <lexiongjia@gmail.com>
2021-07-05 22:45:27 +0800

    test c11

--- ashen/src/ash_log.c
+++ ashen/src/ash_log.c
@@ -5,16 +5,24 @@
 #include "ash_config.h"
 #include "ash_general.h"
 #include "ash_log.h"
+#include <stdlib.h>
+#include <stdnoreturn.h>
 
 struct _ash_log_context {
   ash_uint_t log_level;
 };
+
+noreturn void exit_test();
+
+noreturn void exit_test() {
+  exit(1);
+}
 
 ash_log_context_t* ash_log_create(void) {
   return ASH_NULL;
 }
 
 void ash_log_destroy(ash_log_context_t *context) {
-
+  exit(1);
 }
 

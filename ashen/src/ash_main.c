dc8101d366ef688c49668cb01b52e2ee793a07a1
xiongjia <lexiongjia@gmail.com>
2021-07-11 18:01:19 +0800

    adding cmake test

--- ashen/src/ash_main.c
+++ ashen/src/ash_main.c
@@ -1,18 +1,22 @@
 /*
  */
 
+#if 0
 #include "ash_config.h"
 #include "ash_general.h"
 #include "ash_log.h"
+#endif
+
 #include <stdio.h>
 
 int main(const int argc, const char **argv) {
+#if 0
   ash_log_context_t *log_context = ASH_NULL;
-
   printf("test, os type %s\n", ASH_OS_TYPE);
-
   log_context = ash_log_create();
   ash_log_destroy(log_context);
+#endif
+  printf("test\n");
   return 0;
 }
 

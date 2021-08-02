da39ac0ef11a857327b7d94738acd0010651d0b0
xiongjia <lexiongjia@gmail.com>
2021-08-02 22:50:31 +0800

    adding tests

--- ashen/src/ash_palloc.c
+++ ashen/src/ash_palloc.c
@@ -4,4 +4,24 @@
 
 #include "ash_palloc.h"
 
+typedef struct _ash_pool_data_t ash_pool_data_t;
+typedef struct _ash_pool_data_large_t ash_pool_data_large_t;
+typedef struct _ash_pool_t ash_pool_t;
+
+struct _ash_pool_data_t {
+  unsigned char *last;
+  unsigned char *end;
+  ash_pool_data_t *next;
+};
+
+struct _ash_pool_data_large_t {
+  void *alloc;
+  ash_pool_data_large_t *next;
+};
+
+struct _ash_pool_t {
+  ash_pool_data_t data;
+  ash_pool_data_large_t *large;
+  ash_pool_t *current;
+};
 
--- ashen/src/ash_palloc.h
+++ ashen/src/ash_palloc.h
@@ -4,6 +4,12 @@
 #ifndef _ASH_PALLOC_H_
 #define _ASH_PALLOC_H_ 1
 
+#include "ash_general.h"
+
+typedef struct _ash_pool_t ash_pool_t;
+
+
+
 
 #endif /* !defined(_ASH_PALLOC_H_) */
 

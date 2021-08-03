ef9f135e36802eb387134c817998773f3df5b25d
xiongjia <lexiongjia@gmail.com>
2021-08-04 00:08:01 +0800

    updating tests

--- ashen/src/ash_log.h
+++ ashen/src/ash_log.h
@@ -4,6 +4,7 @@
 #ifndef _ASH_LOG_H_
 #define _ASH_LOG_H_ 1
 
+
 ASH_DECLARE(void) ash_log_append(const char *src, const int src_line,
                                  int level, const char *fmt, ...);
 

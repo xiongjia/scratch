c70f2af08f6395dc64075115bd70f04a2838e46a
xiongjia <lexiongjia@gmail.com>
2021-08-08 21:35:50 +0800

    update tests

--- ashen/src/ash_log.c
+++ ashen/src/ash_log.c
@@ -6,11 +6,9 @@
 #include "ash_general.h"
 #include "ash_log.h"
 #include <stdlib.h>
-#include <stdnoreturn.h>
 
-ASH_DECLARE(void) ash_log_append(const char *src, const int src_line,
-                                 int level, const char *fmt, ...) {
-  return;
+ASH_DECLARE_CDECL(void) ash_log_append(const char *src, const int src_line,
+                                       int level, int flags, const char *fmt, ...) {
 }
 
 
--- ashen/src/ash_log.h
+++ ashen/src/ash_log.h
@@ -4,9 +4,10 @@
 #ifndef _ASH_LOG_H_
 #define _ASH_LOG_H_ 1
 
+#include "ash_general.h"
 
-ASH_DECLARE(void) ash_log_append(const char *src, const int src_line,
-                                 int level, const char *fmt, ...);
+ASH_DECLARE_CDECL(void) ash_log_append(const char *src, const int src_line,
+                                       int level, int flags, const char *fmt, ...);
 
 #endif /* !defined(_ASH_LOG_H_) */
 

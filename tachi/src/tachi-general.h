755da1219cb8f5e162625b41def79a95edcb2ec3
xiongjia <lexiongjia@gmail.com>
2022-05-15 18:00:34 +0800

    add mm

--- tachi/src/tachi-general.h
+++ tachi/src/tachi-general.h
@@ -4,10 +4,12 @@
 #ifndef _TACHI_GENERAL_H_
 #define _TACHI_GENERAL_H_ 1
 
+#include <stdlib.h>
+
+typedef unsigned __int64 size_t;
+
 typedef unsigned char uchar_t;
 typedef char          char_t;
-
-
 typedef unsigned long uint32_t;
 typedef long          int32_t;
 
---
+++ tachi/src/tachi-mm.h
@@ -0,0 +1,13 @@
+/**
+ */
+
+#ifndef _TACHI_MM_H_
+#define _TACHI_MM_H_ 1
+
+#include "tachi-general.h"
+
+#define tachi_malloc(_sz) malloc(_size);
+#define tachi_free(_ptr)  free(_ptr);
+
+
+#endif /* !defined(_TACHI_MM_H_) */

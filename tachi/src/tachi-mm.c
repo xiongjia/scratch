ec11784b0a1668987a41918d53742a2b9af86fe4
xiongjia <lexiongjia@gmail.com>
2022-05-16 22:37:40 +0800

    updating mm

--- tachi/src/tachi-mm.c
+++ tachi/src/tachi-mm.c
@@ -3,3 +3,22 @@
  */
 
 #include "tachi-mm.h"
+
+
+typedef struct _tachi_pool_data {
+  uchar_t *hdr;
+  uchar_t *last;
+  uchar_t* current;
+
+  tachi_pool *next;
+} tachi_pool_data;
+
+struct _tachi_pool {
+  tachi_pool_data *data;
+};
+
+
+tachi_pool* tachi_create_pool(void) {
+
+  return NULL;
+}
--- tachi/src/tachi-mm.h
+++ tachi/src/tachi-mm.h
@@ -6,8 +6,13 @@
 
 #include "tachi-general.h"
 
+typedef struct _tachi_pool tachi_pool;
+
 #define tachi_malloc(_sz) malloc(_size);
+
 #define tachi_free(_ptr)  free(_ptr);
 
+
+tachi_pool* tachi_create_pool(void);
 
 #endif /* !defined(_TACHI_MM_H_) */

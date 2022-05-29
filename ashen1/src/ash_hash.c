64f15b53b82bf2a04a321843f2cb996d1a26b025
xiongjia <lexiongjia@gmail.com>
2022-05-29 13:24:30 +0800

    updating hash function

--- ashen1/src/ash_hash.c
+++ ashen1/src/ash_hash.c
@@ -3,10 +3,20 @@
 
 #include "ash_hash.h"
 
-uint32_t ash_hash_times33(const char *key, uint32_t klen, uint32_t hash) {
+uint32_t ash_hash_dbj(const char *key, uint32_t klen, uint32_t hash) {
   uint32_t hash_val = hash;
-  for (uint32_t i = 0; i < klen; i++) {
+  uint32_t i;
+
+  /* An algorithm produced by Professor Daniel J. Bernstein and shown 
+   * first to the world on the usenet newsgroup comp.lang.c. It is one of 
+   * the most efficient hash functions ever published.
+   */
+  for (i = 0; i < klen; i++) {
     hash = ((hash << 5) + hash) + (uint32_t)key[i];
   }
   return hash; 
+}
+
+uint32_t ash_hash_dbj_default(const char *key, uint32_t klen) {
+  return ash_hash_dbj(key, klen, 5381);
 }
--- ashen1/src/ash_hash.h
+++ ashen1/src/ash_hash.h
@@ -7,6 +7,9 @@
 #include "ash_types.h"
 #include "ash_mm.h"
 
-uint32_t ash_hash_times33(const char *key, uint32_t klen, uint32_t hash);
+uint32_t ash_hash_dbj(const char *key, uint32_t klen, uint32_t hash);
+
+uint32_t ash_hash_dbj_default(const char *key, uint32_t klen);
+
 
 #endif /* !defined(_ASH_HASH_H_) */

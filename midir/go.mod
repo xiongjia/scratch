8c215a7a05cfa861094c569c995af30864afafc5
xiongjia <lexiongjia@gmail.com>
2022-10-17 22:48:48 +0800

    updating golang test

--- midir/go.mod
+++ midir/go.mod
@@ -1,3 +1,12 @@
 module midir
 
 go 1.18
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/stretchr/testify v1.8.0 // indirect
+	golang.org/x/exp v0.0.0-20221012211006-4de253d81b95 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- midir/leetcode/p0001.two_sum_test.go
+++ midir/leetcode/p0001.two_sum_test.go
@@ -1,10 +1,26 @@
 package leetcode
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_p0001(t *testing.T) {
-	fmt.Printf("%v\n", TwoSumSolution1([]int{3, 2, 4}, 6))
+	t.Log("P001 Two Sum")
+
+	cases := []struct {
+		nums     []int
+		target   int
+		expected []int
+	}{
+		{[]int{3, 2, 4}, 6, []int{1, 2}},
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+	}
+	for _, c := range cases {
+		r := TwoSumSolution1(c.nums, c.target)
+		t.Logf("Source = %v, Target = %v. Result = %v, Expected = %v",
+			c.nums, c.target, r, c.expected)
+		assert.ElementsMatch(t, r, c.expected)
+	}
 }

52d87a7127847a9a00cdbba313ccf02ec4bd6d1e
xiongjia <lexiongjia@gmail.com>
2022-10-17 23:00:26 +0800

    updating unit test

--- midir/leetcode/p0001.two_sum_test.go
+++ midir/leetcode/p0001.two_sum_test.go
@@ -10,13 +10,13 @@ func Test_p0001(t *testing.T) {
 	t.Log("P001 Two Sum")
 
 	cases := []struct {
-		nums     []int
-		target   int
-		expected []int
+		nums, expected []int
+		target         int
 	}{
-		{[]int{3, 2, 4}, 6, []int{1, 2}},
-		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{[]int{3, 2, 4}, []int{1, 2}, 6},
+		{[]int{2, 7, 11, 15}, []int{0, 1}, 9},
 	}
+
 	for _, c := range cases {
 		r := TwoSumSolution1(c.nums, c.target)
 		t.Logf("Source = %v, Target = %v. Result = %v, Expected = %v",

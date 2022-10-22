51d9fbaa50c1da97cb6b71e1b2f54dabddbc1c53
xiongjia <lexiongjia@gmail.com>
2022-10-22 23:34:17 +0800

    adding p0003

--- midir/leetcode/p0003.longest_substr_without_repeating_char.go
+++ midir/leetcode/p0003.longest_substr_without_repeating_char.go
@@ -4,6 +4,22 @@ func LengthOfLongestSubstringSolution1(s string) int {
 	if len(s) == 0 {
 		return 0
 	}
-
-	return 0
+	var bitSet [256]bool
+	result, left, right := 0, 0, 0
+	for left < len(s) {
+		if bitSet[s[right]] {
+			bitSet[s[left]] = false
+			left++
+		} else {
+			bitSet[s[right]] = true
+			right++
+		}
+		if result < right-left {
+			result = right - left
+		}
+		if left+result >= len(s) || right >= len(s) {
+			break
+		}
+	}
+	return result
 }
--- midir/leetcode/p0003.longest_substr_without_repeating_char_test.go
+++ midir/leetcode/p0003.longest_substr_without_repeating_char_test.go
@@ -2,8 +2,21 @@ package leetcode
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_p0003(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected int
+	}{
+		{input: "abcabcbb", expected: 3},
+	}
 
+	for _, c := range cases {
+		r := LengthOfLongestSubstringSolution1(c.input)
+		t.Logf("input = %s, expected = %d; r = %d\n", c.input, c.expected, r)
+		assert.Equal(t, r, c.expected)
+	}
 }

938327ff6699308a9bdd43e3b138411fc4c1ed49
xiongjia <lexiongjia@gmail.com>
2022-10-24 21:51:53 +0800

    adding more solution

---
+++ midir/leetcode/p0004.median_of_two_sorted_arrays.go
@@ -0,0 +1,6 @@
+package leetcode
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	// TODO
+	return 0.0
+}
---
+++ midir/leetcode/p0004.median_of_two_sorted_arrays_test.go
@@ -0,0 +1,7 @@
+package leetcode
+
+import "testing"
+
+func Test_p0004(t *testing.T) {
+	// TODO
+}

d58e7af49a45b43bc59dfec92515d5fb2aa7c2cf
xiongjia <lexiongjia@gmail.com>
2023-01-30 22:31:08 +0800

    updating midir

--- midir/leetcode/p0004.median_of_two_sorted_arrays.go
+++ midir/leetcode/p0004.median_of_two_sorted_arrays.go
@@ -1,6 +1,10 @@
 package leetcode
 
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+func FindMedianSortedArraysSolution1(nums1 []int, nums2 []int) float64 {
+	if len(nums1) > len(nums2) {
+		return FindMedianSortedArraysSolution1(nums2, nums1)
+	}
+
 	// TODO
 	return 0.0
 }

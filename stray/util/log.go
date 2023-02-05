550314bb02757aedab08e2878983b796f98d5dcc
xiongjia <lexiongjia@gmail.com>
2023-02-05 19:02:16 +0800

    updating tests

--- stray/util/log.go
+++ stray/util/log.go
@@ -2,19 +2,63 @@ package util
 
 import (
 	"fmt"
+	"strconv"
+)
+
+type Level int8
+
+const (
+	TraceLevel Level = iota
+	DebugLevel
+	InfoLevel
+	WarnLevel
+	ErrorLevel
+	PanicLevel
+	FatalLevel
 )
 
 type Logger interface {
-	Debugf(format string)
+	Debugf(format string, args ...interface{})
 }
 
 type logger struct {
 	name string
 }
 
-func (log *logger) Debugf(format string) {
+func (l Level) String() string {
+	switch l {
+	case TraceLevel:
+		return "TRACE"
+	case DebugLevel:
+		return "DEBUG"
+	case InfoLevel:
+		return "INFO"
+	case WarnLevel:
+		return "WARN"
+	case ErrorLevel:
+		return "ERROR"
+	case PanicLevel:
+		return "PANIC"
+	case FatalLevel:
+		return "FATAL"
+	default:
+		return strconv.Itoa(int(l))
+	}
+}
+
+func (log *logger) Debugf(format string, args ...interface{}) {
 	fmt.Println(log.name)
+	log.write(format, args...)
+}
+
+func (log *logger) write(format string, args ...interface{}) {
 	fmt.Println(format)
+	for _, arg := range args {
+		switch arg.(type) {
+		case string:
+			fmt.Printf("string = %v\n", arg)
+		}
+	}
 }
 
 func newLogger() Logger {
@@ -25,6 +69,8 @@ func newLogger() Logger {
 func Test() {
 	fmt.Println("test1")
 
+	fmt.Printf("test data = %d, %v\n", TraceLevel, DebugLevel.String())
+
 	log := newLogger()
-	log.Debugf("abc")
+	log.Debugf("abc", "1", "2", "3")
 }

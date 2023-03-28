50779a520aea1cef3bd893b565525e0b9711aa15
xiongjia <lexiongjia@gmail.com>
2023-03-28 21:48:04 +0800

    updating http test

--- stray/cmd/stray/main.go
+++ stray/cmd/stray/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -12,6 +13,7 @@ type Context struct {
 }
 
 func welcome(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Printf("request test\n")
 	age := r.URL.Query().Get("age")
 	fmt.Fprintf(w, "hello %s - %s\n", ps.ByName("name"), age)
 }
@@ -22,6 +24,12 @@ func main() {
 
 	router := httprouter.New()
 	router.GET("/hello/:name", welcome)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/hello/ut?age=1", nil)
+	router.ServeHTTP(w, req)
+	fmt.Printf("code = %d, %s", w.Code, w.Body.String())
+
 	fmt.Printf("Server starting at port %d ...\n", ctx.serverPort)
 	http.ListenAndServe(fmt.Sprintf(":%d", ctx.serverPort), router)
 }

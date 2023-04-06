4ec26587e5b4288727cd3c6851ae75376ea5c212
xiongjia <lexiongjia@gmail.com>
2023-04-06 21:50:48 +0800

    updating tests

--- stray/cmd/stray/main.go
+++ stray/cmd/stray/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -10,6 +12,7 @@ import (
 
 type Context struct {
 	serverPort int16
+	test       func()
 }
 
 func welcome(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -18,9 +21,38 @@ func welcome(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "hello %s - %s\n", ps.ByName("name"), age)
 }
 
+func test1() {
+	fmt.Println("test func1")
+}
+
 func main() {
-	ctx := &Context{}
-	ctx.serverPort = 8871
+	dbgData := &struct {
+		a byte
+		b byte
+		c int32
+	}{1, 1, 3}
+
+	fmt.Printf("Test %#v\n", dbgData)
+
+	dbgBuf := &bytes.Buffer{}
+	err := binary.Write(dbgBuf, binary.BigEndian, dbgData)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(dbgBuf.Bytes())
+
+	fmt.Println(`abc"abc"\n`)
+
+	data1 := [...]int{0, 1, 2}
+	s1 := copy(data1[:1], data1[:2])
+	data1[0] = 100
+	fmt.Printf("data1 %v, s1 %v\n", data1, s1)
+
+	ctx := &Context{
+		serverPort: 8871,
+		test:       test1,
+	}
+	ctx.test()
 
 	router := httprouter.New()
 	router.GET("/hello/:name", welcome)

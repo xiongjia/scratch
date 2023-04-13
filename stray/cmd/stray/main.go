e41e0c4773c3acf422aa533814099e3cef9bf6ea
xiongjia <lexiongjia@gmail.com>
2023-04-14 00:47:48 +0800

    updating test

--- stray/cmd/stray/main.go
+++ stray/cmd/stray/main.go
@@ -28,7 +28,9 @@ func test1() {
 }
 
 func main() {
-	util.Test()
+	logger := util.NewLogger(util.DebugLevel)
+	logger.Debug("Debug 1")
+	logger.Debugf("Debug %d", 999)
 
 	dbgData := &struct {
 		a byte
--- stray/util/log.go
+++ stray/util/log.go
@@ -1,109 +1,38 @@
 package util
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"os"
 )
 
-type Level int8
+type LogAppenderf func(format string, v ...any)
+type LogAppender func(v ...any)
+
+type LogLevel int8
 
 const (
-	TraceLevel Level = iota
+	TraceLevel LogLevel = iota
 	DebugLevel
 	InfoLevel
 	WarnLevel
 	ErrorLevel
-	PanicLevel
-	FatalLevel
 )
 
-type Logger interface {
-	Debugf(format string, args ...interface{})
+type Logger struct {
+	Debugf LogAppenderf
+	Debug  LogAppender
 }
 
-type Options struct {
-	enable       bool
-	enableStdout bool
-	level        Level
-
-	logFilename     string
-	logFileMaxSize  int
-	rotatMaxSize    int
-	rotatMaxBackups int
-	rotateMaxDays   int
-	rotateCompress  bool
+func makeLogWrite(confLevel LogLevel, level LogLevel, prefix string) (LogAppenderf, LogAppender) {
+	appender := log.New(os.Stdout, prefix, log.Ldate|log.Ltime|log.Lshortfile)
+	return appender.Printf, appender.Print
 }
 
-type logger struct {
-	level       Level
-	fileWriter  io.Writer
-	printWriter io.Writer
-}
-
-func (l Level) String() string {
-	switch l {
-	case TraceLevel:
-		return "TRACE"
-	case DebugLevel:
-		return "DEBUG"
-	case InfoLevel:
-		return "INFO"
-	case WarnLevel:
-		return "WARN"
-	case ErrorLevel:
-		return "ERROR"
-	case PanicLevel:
-		return "PANIC"
-	case FatalLevel:
-		return "FATAL"
-	default:
-		return fmt.Sprintf("LEVEL(%d)", l)
+func NewLogger(level LogLevel) *Logger {
+	debugAppenderf, debugAppender := makeLogWrite(level, DebugLevel, "DEBUG ")
+	logger := &Logger{
+		Debugf: debugAppenderf,
+		Debug:  debugAppender,
 	}
-}
-
-func (log *logger) Debugf(format string, args ...interface{}) {
-	log.write(DebugLevel, format, args...)
-}
-
-func (log *logger) write(level Level, format string, args ...interface{}) {
-	msg := fmt.Sprintf(format, args...)
-	if log.printWriter != nil {
-		log.printWriter.Write([]byte(msg))
-		log.printWriter.Write([]byte("\n"))
-	}
-}
-
-func newLogger(opts *Options) Logger {
-	log := &logger{
-		level:      opts.level,
-		fileWriter: nil,
-		printWriter: func() io.Writer {
-			if opts.enableStdout {
-				return os.Stdout
-			} else {
-				return nil
-			}
-		}(),
-	}
-	return log
-}
-
-func Test() {
-	infoLog := log.New(os.Stdout, "INFO", log.Ldate|log.Ltime|log.Lshortfile)
-	infoLog.Println("test1")
-	infoLog.Printf("abc %s", "data")
-	infoLog.Print("aaa")
-	infoLog.Print("aaa1")
-
-	opts := &Options{}
-	fmt.Printf("%v\n", opts.enable)
-
-	log := newLogger(&Options{
-		enable:       true,
-		level:        DebugLevel,
-		enableStdout: true,
-	})
-	log.Debugf("Test log %s %s %s", "1", "2", "3")
+	return logger
 }

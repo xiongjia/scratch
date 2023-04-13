60253480c02b7aa6d8987a51d4da7ca695370b06
xiongjia <lexiongjia@gmail.com>
2023-04-14 01:28:06 +0800

    updating

--- stray/util/log.go
+++ stray/util/log.go
@@ -1,8 +1,11 @@
 package util
 
 import (
+	"io"
 	"log"
 	"os"
+
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 type LogAppenderf func(format string, v ...any)
@@ -23,16 +26,29 @@ type Logger struct {
 	Debug  LogAppender
 }
 
-func makeLogWrite(confLevel LogLevel, level LogLevel, prefix string) (LogAppenderf, LogAppender) {
+func makeAppender(confLevel LogLevel, level LogLevel, prefix string, w io.Writer) (LogAppenderf, LogAppender) {
 	appender := log.New(os.Stdout, prefix, log.Ldate|log.Ltime|log.Lshortfile)
+	appender.SetOutput(w)
 	return appender.Printf, appender.Print
 }
 
-func NewLogger(level LogLevel) *Logger {
-	debugAppenderf, debugAppender := makeLogWrite(level, DebugLevel, "DEBUG ")
-	logger := &Logger{
-		Debugf: debugAppenderf,
-		Debug:  debugAppender,
+func makeWriter(f string) io.Writer {
+	if f == "" {
+		return os.Stdout
+	} else {
+		return &lumberjack.Logger{
+			Filename:   f,
+			MaxSize:    500, // megabytes
+			MaxBackups: 3,
+			MaxAge:     28,   //days
+			Compress:   true, // disabled by default
+		}
 	}
+}
+
+func NewLogger(filename string, level LogLevel) *Logger {
+	writer := makeWriter(filename)
+	logger := &Logger{}
+	logger.Debugf, logger.Debug = makeAppender(level, DebugLevel, "DEBUG ", writer)
 	return logger
 }

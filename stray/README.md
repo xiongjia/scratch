21fb96fd51a0634754e1710f734c2c4e2eb153e7
xiongjia <lexiongjia@gmail.com>
2023-11-06 18:12:00 +0800

    adding swagger

--- stray/README.md
+++ stray/README.md
@@ -4,3 +4,8 @@
 - cd api
 - buf generate
 
+## Swagger 
+- ui  go-bindata --nocompress -pkg ui -o generated/swagger/ui/ui.go -prefix third_party third_party/swagger-ui/...
+- data  go-bindata --nocompress -pkg data -o generated/swagger/data/data.go -prefix generated/api/swagger generated/api/swagger/...
+	
+	
--- stray/cmd/stray/main.go
+++ stray/cmd/stray/main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
-
 	"context"
 	"log"
 	"net"
 	"net/http"
 
-	"stray/util"
-
 	hello "stray/generated/api/proto/v1"
+	// swaggerdata "stray/generated/swagger/data"
+	// swaggerui "stray/generated/swagger/ui"
+	// assetfs "github.com/elazarl/go-bindata-assetfs"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
@@ -31,7 +28,21 @@ func (s *server) SayHello(ctx context.Context, in *hello.HelloRequest) (*hello.H
 	return &hello.HelloReply{Message: in.Name + " test123 "}, nil
 }
 
+func localTest() {
+	var x int
+	x = 1
+
+	defer func() {
+		log.Println("x (defer) =", x)
+	}()
+
+	log.Println("x =", x)
+	x = 101
+}
+
 func main() {
+	localTest()
+
 	listen, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalln("Failed to listen:", err)
@@ -65,7 +76,28 @@ func main() {
 		Handler: gwmux,
 	}
 
-	logger.Debugf("Log level = %s", util.LogLevelToStr(conf.LogLevel))
+	// datafs := &assetfs.AssetFS{
+	// 	Asset:    swaggerdata.Asset,
+	// 	AssetDir: swaggerdata.AssetDir,
+	// }
+	// serveFileFn := func(w http.ResponseWriter, r *http.Request, root *assetfs.AssetFS, path string) error {
+	// 	file, err := root.Open(path)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	defer file.Close()
+	// 	http.ServeContent(w, r, filepath.Base(path), time.Time{}, file)
+	// 	return nil
+	// }
+
+	// uiServer := http.FileServer(&assetfs.AssetFS{
+	// 	Asset:    swaggerui.Asset,
+	// 	AssetDir: swaggerui.AssetDir,
+	// 	Prefix:   "swagger-ui",
+	// 	Fallback: "index.html",
+	// })
+	// gwmux.Handle("/swagger-ui/", http.StripPrefix("/swagger-ui/", uiServer))
+
 	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
 	log.Fatalln(gwServer.ListenAndServe())
 }

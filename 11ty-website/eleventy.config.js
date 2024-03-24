a50f2b9986ef0b7f0da468dab7e633bf90f9b795
xiongjia <lexiongjia@gmail.com>
2024-03-24 19:40:49 +0800

    html mini plugin

--- 11ty-website/eleventy.config.js
+++ 11ty-website/eleventy.config.js
@@ -3,27 +3,26 @@
 const htmlmin = require('html-minifier')
 
 module.exports = (cfg) => {
-  // cfg.addTransform('htmlmin', (content) => {
-  //   if (!this.page.outputPath) {
-  //     return content
-  //   }
-  //   if (!this.page.outputPath.endsWith('.html')) {
-  //     return content
-  //   }
-  //   return htmlmin.minify(content, {
-  //     collapseWhitespace: true,
-  //     useShortDoctype: true,
-  //   })
-  // })
+  cfg.addTransform('async-htmlmin', async (content, outputPath) => {
+    if (outputPath.toLowerCase().endsWith('.html')) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      })
+    } else {
+      return content
+    }
+  })
 
   return {
     dir: {
       input: 'src',
       output: 'dist',
     },
-    // templateFormats: ['md', 'html'],
-    // markdownTemplateEngine: 'njk',
-    // dataTemplateEngine: 'njk',
-    // htmlTemplateEngine: 'njk',
+    templateFormats: ['md', 'html'],
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
   }
 }

3ccb6449378c6070655d8604d3c790d66a9bedc0
xiongjia <lexiongjia@gmail.com>
2024-08-18 21:51:56 +0800

    swr test

--- projects/pea/pkg/components/src/Guide/StepHostInput.tsx
+++ projects/pea/pkg/components/src/Guide/StepHostInput.tsx
@@ -1,11 +1,40 @@
 import { Space } from 'antd'
 import React, { useContext } from 'react'
+import useSWR from 'swr'
 import { GuideContext } from './GuideContext'
 import IpRangeForm from './IpRangeForm'
 import SshAccountForm from './SshAccountForm'
 
+function delayTest(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+type MockData = {
+  name: string
+  data: string
+}
+
+const fetcher = async (args: string): Promise<MockData> => {
+  console.log('load data ', args)
+  await delayTest(1000 * 15)
+  console.log('return data ', args)
+  return { name: args, data: 'b' }
+}
+
 const StepHostInput = () => {
   const ctx = useContext(GuideContext)
+  const { data, error, isLoading, isValidating } = useSWR<MockData>(
+    '/api/user/1211',
+    fetcher,
+    {
+      keepPreviousData: false,
+    },
+  )
+
+  console.log('data', data)
+  console.log('error', error)
+  console.log('isLoading', isLoading)
+  console.log('isValidating', isValidating)
 
   return (
     <Space direction="vertical">
@@ -17,6 +46,12 @@ const StepHostInput = () => {
         sshUsernameInitValue={ctx?.sshUsername}
         sshPasswordInitValue={ctx?.sshPassword}
       />
+
+      {isLoading ?? <div>loading...</div>}
+
+      <div>
+        hello [{data?.name}, {data?.data}]
+      </div>
     </Space>
   )
 }

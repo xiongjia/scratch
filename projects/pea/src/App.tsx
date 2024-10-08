4068014dc87c767315cda441d97780bc48e2a6a9
xiongjia <lexiongjia@gmail.com>
2024-10-09 00:17:02 +0800

    updating prettier rc

--- projects/pea/src/App.tsx
+++ projects/pea/src/App.tsx
@@ -1,13 +1,13 @@
-import {Button}  from './components/button'
 import './App.css'
+import { Button } from './components/button'
 
 function App() {
   return (
     <>
-     <p>Test</p> <br />
+      <p>Test</p> <br />
       <Button label="Test Btn" primary />
     </>
   )
-};
+}
 
 export default App

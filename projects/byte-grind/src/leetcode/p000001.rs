d6a15a18b70f0d188b79893be388b03cb8736eb3
xiongjia <lexiongjia@gmail.com>
2025-02-23 13:51:40 +0800

    adding rust tests projector

--- projects/byte-grind/src/leetcode/p000001.rs
+++ projects/byte-grind/src/leetcode/p000001.rs
@@ -1,45 +1,58 @@
-/**
- * [1] Two Sum
+// Problem: https://leetcode.com/problems/two-sum/
+
+/*
+ * 1. Two Sum
  *
- * Given an array of integers, return indices of the two numbers such that they
- * add up to a specific target.
+ * [easy]
  *
- * You may assume that each input would have exactly one solution, and you may
- * not use the same element twice.
+ * Given an array of integers nums and an integer target, return indices of
+ * the two numbers such that they add up to target.
  *
- * Example:
+ * You may assume that each input would have exactly one solution, and you
+ * may not use the same element twice.
+ * You can return the answer in any order.
  *
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
  *
- * Given nums = [2, 7, 11, 15], target = 9,
+ * Example 2:
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
  *
- * Because nums[0] + nums[1] = 2 + 7 = 9,
- * return [0, 1].
+ * Example 3:
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
  *
+ * Constraints:
+ * = 2 <= nums.length <= 104
+ * = -109 <= nums[i] <= 109
+ * = -109 <= target <= 109
+ * = Only one valid answer exists.
  */
+
 pub struct Solution {}
 
-// problem: https://leetcode.com/problems/two-sum/
-// discuss: https://leetcode.com/problems/two-sum/discuss/?currentPage=1&orderBy=most_votes&query=
-
-// submission codes start here
-
 use std::collections::HashMap;
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut map = HashMap::with_capacity(nums.len());
+
         for (index, num) in nums.iter().enumerate() {
             match map.get(&(target - num)) {
                 None => {
                     map.insert(num, index);
                 }
-                Some(sub_index) => return vec![*sub_index as i32, index as i32],
+                Some(sub_index) => {
+                    return vec![*sub_index as i32, index as i32];
+                }
             }
         }
         vec![]
     }
 }
-
-// submission codes end
 
 #[cfg(test)]
 mod tests {

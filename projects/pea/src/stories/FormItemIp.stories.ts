97996c7718021fecda10b9bb16ecb047c382abc7
xiongjia <lexiongjia@gmail.com>
2025-05-25 14:41:50 +0800

    adding ts-node tests

--- projects/pea/src/stories/FormItemIp.stories.ts
+++ projects/pea/src/stories/FormItemIp.stories.ts
@@ -1,6 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { InputIp } from '&/component/input'
 
+const inputStatus = {
+  Error: 'error',
+  Warn: 'warning',
+  Normal: '',
+}
+
 const meta: Meta<typeof InputIp> = {
   title: 'Example/Input IP',
   component: InputIp,
@@ -9,6 +15,18 @@ const meta: Meta<typeof InputIp> = {
       name: 'placeholder lab',
       control: 'text',
       description: 'placeholder desc',
+    },
+    status: {
+      options: Object.keys(inputStatus),
+      mapping: inputStatus,
+      control: {
+        type: 'select',
+        labels: {
+          Error: 'error',
+          Warn: 'warning',
+          Normal: '',
+        },
+      },
     },
   },
 }

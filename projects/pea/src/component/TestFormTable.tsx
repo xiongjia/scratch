a1cf4cf5e3f24ad23b2a28c3d125568fdecc1f63
xiongjia <lexiongjia@gmail.com>
2025-06-03 23:54:56 +0800

    test proform

--- projects/pea/src/component/TestFormTable.tsx
+++ projects/pea/src/component/TestFormTable.tsx
@@ -4,6 +4,7 @@ import {
   ProForm,
   ProFormText,
 } from '@ant-design/pro-components'
+import { Button } from 'antd'
 
 type TestTableItem = {
   id: number
@@ -36,8 +37,23 @@ const TestFormTable = () => {
           console.log('finsh proform', val)
           return true
         }}
+        onValuesChange={(val: TestFormData) => {
+          console.log('proform on change', val)
+        }}
         initialValues={{
           username: '123',
+        }}
+        layout="inline"
+        submitter={{
+          resetButtonProps: { style: { display: 'none' } },
+          submitButtonProps: {},
+          render: (props): React.ReactNode[] => {
+            return [
+              <Button key="submit" onClick={() => props.form?.submit?.()}>
+                Ok submit 1
+              </Button>,
+            ]
+          },
         }}
       >
         <ProFormText name={'username'} />

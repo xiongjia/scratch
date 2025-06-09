95071a6d3e2e26fe03ae662587125f6fb9bbebf8
xiongjia <lexiongjia@gmail.com>
2025-06-09 23:49:45 +0800

    adding ts-node tests

--- projects/pea/src/component/TestButton.tsx
+++ projects/pea/src/component/TestButton.tsx
@@ -1,8 +1,47 @@
 import { Button } from 'antd'
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
 
-const TestButton = () => {
+const queryClient = new QueryClient()
+
+const wait = (sec: number) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Done waiting')
+      resolve(sec)
+    }, sec * 1000)
+  })
+}
+
+const TestData = () => {
+  const { error, data, isFetching } = useQuery({
+    queryKey: ['todos'],
+    queryFn: async () => {
+      const testData = {
+        a: 1,
+      }
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          console.log('Done waiting')
+          resolve(JSON.stringify(testData))
+        }, 10 * 1000)
+      })
+    },
+  })
+
+  if (isFetching) {
+    console.log('isFetching')
+  }
+  if (error) {
+    console.log('err', error)
+  }
   return (
     <>
+      <p>{isFetching ? 'fetching' : data}</p>
+      <br />
       <Button
         onClick={() => {
           console.log('test')
@@ -10,6 +49,16 @@ const TestButton = () => {
       >
         Test Button
       </Button>
+    </>
+  )
+}
+
+const TestButton = () => {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <TestData />
+      </QueryClientProvider>
     </>
   )
 }

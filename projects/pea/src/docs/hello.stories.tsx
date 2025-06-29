d9e7a04020203c7ecbcbb4977d833a552d02a662
xiongjia <lexiongjia@gmail.com>
2025-06-29 17:34:32 +0800

    refactory project

--- projects/pea/src/docs/hello.stories.tsx
+++ projects/pea/src/docs/hello.stories.tsx
@@ -1,3 +1,4 @@
+import '@ant-design/v5-patch-for-react-19'
 import { TestButton } from '&/component'
 
 export const Test1 = () => {
--- projects/pea/src/main.tsx
+++ projects/pea/src/main.tsx
@@ -1,3 +1,4 @@
+import '@ant-design/v5-patch-for-react-19'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
